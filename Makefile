8a80bd6ac1216dd54e10e032ca810a1cfad3b9f5
m_utkej <utkej1@gmail.com>
2004-01-22 14:05:57 -0600

    bk-changeset-1.30.1.6 m_utkej@wally.mynet.test|ChangeSet|20040122200557|10465 ChangeSet   1.30.1.6 04/01/22 14:05:57 m_utkej@wally.mynet.test +9 -0   add a directory with initial stage of a roundtrip test   add range check for index into AliasMap   return non-zero for any caught exception in tests
      testRoundTrip/setenv.sh
        1.2 04/01/22 14:05:56 m_utkej@wally.mynet.test +2 -10
        baseline
    
      testRoundTrip/Makefile
        1.2 04/01/22 14:05:56 m_utkej@wally.mynet.test +66 -13
        baseline
    
      system/src/AliasMap.cpp
        1.8.1.1 04/01/22 14:05:56 m_utkej@wally.mynet.test +31 -3
        want to prevent SEGV for non-existing Alias keys
    
      algorithms/BasicBlockPreaccumulation/test/t.cpp
        1.3 04/01/22 14:05:56 m_utkej@wally.mynet.test +1 -0
        needs to return non-zero to stop make
    
      Makefile
        1.5 04/01/22 14:05:56 m_utkej@wally.mynet.test +1 -0
        add this, so far only for the 'clean' target 
    
      testRoundTrip/README
        1.1 04/01/22 14:02:28 m_utkej@wally.mynet.test +15 -0
    
      testRoundTrip/README
        1.0 04/01/22 14:02:28 m_utkej@wally.mynet.test +0 -0
        BitKeeper file /m_home/m_utkej/Argonne/bk/xaifBooster_current/xaifBooster/testRoundTrip/README
    
      testRoundTrip/simple1.f
        1.1 04/01/22 13:57:26 m_utkej@wally.mynet.test +23 -0
    
      testRoundTrip/simple1.f
        1.0 04/01/22 13:57:26 m_utkej@wally.mynet.test +0 -0
        BitKeeper file /m_home/m_utkej/Argonne/bk/xaifBooster_current/xaifBooster/testRoundTrip/simple1.f
    
      testRoundTrip/w2f__types.f
        1.1 04/01/22 11:54:36 m_utkej@wally.mynet.test +14 -0
    
      testRoundTrip/w2f__types.f
        1.0 04/01/22 11:54:36 m_utkej@wally.mynet.test +0 -0
        BitKeeper file /m_home/m_utkej/Argonne/bk/xaifBooster_current/xaifBooster/testRoundTrip/w2f__types.f
    
      testRoundTrip/setenv.sh
        1.1 04/01/22 11:54:36 m_utkej@wally.mynet.test +10 -0
    
      testRoundTrip/active_module.f
        1.1 04/01/22 11:54:35 m_utkej@wally.mynet.test +104 -0
    
      testRoundTrip/Makefile
        1.1 04/01/22 11:54:35 m_utkej@wally.mynet.test +26 -0
    
      testRoundTrip/setenv.sh
        1.0 04/01/22 11:54:35 m_utkej@wally.mynet.test +0 -0
        BitKeeper file /m_home/m_utkej/Argonne/bk/xaifBooster_current/xaifBooster/testRoundTrip/setenv.sh
    
      testRoundTrip/active_module.f
        1.0 04/01/22 11:54:35 m_utkej@wally.mynet.test +0 -0
        BitKeeper file /m_home/m_utkej/Argonne/bk/xaifBooster_current/xaifBooster/testRoundTrip/active_module.f
    
      testRoundTrip/Makefile
        1.0 04/01/22 11:54:35 m_utkej@wally.mynet.test +0 -0
        BitKeeper file /m_home/m_utkej/Argonne/bk/xaifBooster_current/xaifBooster/testRoundTrip/Makefile

--- Makefile
+++ Makefile
@@ -2,5 +2,6 @@ obj test doc clean:
 	cd utils && $(MAKE) $@
 	cd system && $(MAKE) $@
 	cd algorithms && $(MAKE) $@
+	cd testRoundTrip && $(MAKE) $@
 
 .PHONY: obj test doc clean codegen
---
+++ testRoundTrip/simple1.f
@@ -0,0 +1,23 @@
+      ! A simple program with no control flow
+
+      subroutine head(x, y) 
+        double precision, intent(in) :: x
+        double precision, intent(inout) :: y
+        double precision t1, t2
+        integer i
+
+        i=1
+        t1=sin(x)*y
+        t2=t1*x+i
+        y=t1+t2
+      end subroutine
+      
+      program simple1
+
+      double precision :: x, y
+      x = 10
+      y = 100
+      call head(x, y)
+      write(*,*) y
+
+      end program
