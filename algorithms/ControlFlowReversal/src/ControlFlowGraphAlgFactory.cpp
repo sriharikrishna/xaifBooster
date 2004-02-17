a399d6e121e41ee89b8f8d11e6d9988e74dec0d5
naumann <naumann@float177.mcs.anl-external.org>
2004-02-17 08:50:00 -0600

    bk-changeset-1.30.1.19 naumann@float177.mcs.anl-external.org|ChangeSet|20040217145000|19547 ChangeSet   1.30.1.19 04/02/17 08:50:00 naumann@float177.mcs.anl-external.org +10 -0   algorithm support for control flow graphs
      utils/src/GenericTraverseInvoke.cpp
        1.5 04/02/17 08:50:00 naumann@float177.mcs.anl-external.org +3 -0
        control flow reversal works with new algorithm factory
    
      utils/src/GenericAction.cpp
        1.5 04/02/17 08:50:00 naumann@float177.mcs.anl-external.org +5 -0
        control flow reversal works with new algorithm factory
    
      utils/inc/GenericTraverseInvoke.hpp
        1.5 04/02/17 08:50:00 naumann@float177.mcs.anl-external.org +5 -0
        control flow reversal works with new algorithm factory
    
      utils/inc/GenericAction.hpp
        1.5 04/02/17 08:50:00 naumann@float177.mcs.anl-external.org +2 -1
        control flow reversal works with new algorithm factory
    
      system/src/AlgFactoryManager.cpp
        1.3 04/02/17 08:50:00 naumann@float177.mcs.anl-external.org +1 -1
        control flow reversal works with new algorithm factory
    
      system/inc/ControlFlowGraphAlgBase.hpp
        1.2 04/02/17 08:50:00 naumann@float177.mcs.anl-external.org +2 -2
        control flow reversal works with new algorithm factory
    
      algorithms/Makefile
        1.3 04/02/17 08:50:00 naumann@float177.mcs.anl-external.org +1 -0
        control flow reversal works with new algorithm factory
    
      algorithms/ControlFlowReversal/test/t.cpp
        1.2 04/02/17 08:50:00 naumann@float177.mcs.anl-external.org +2 -0
        control flow reversal works with new algorithm factory
    
      algorithms/ControlFlowReversal/test/Makefile
        1.2 04/02/17 08:50:00 naumann@float177.mcs.anl-external.org +1 -1
        control flow reversal works with new algorithm factory
    
      algorithms/ControlFlowReversal/src/ControlFlowGraphAlgFactory.cpp
        1.2 04/02/17 08:50:00 naumann@float177.mcs.anl-external.org +2 -1
        control flow reversal works with new algorithm factory

--- algorithms/ControlFlowReversal/src/ControlFlowGraphAlgFactory.cpp
+++ algorithms/ControlFlowReversal/src/ControlFlowGraphAlgFactory.cpp
@@ -1,5 +1,6 @@
-#include "xaifBooster/system/inc/ControlFlowGraphAlgFactory.hpp"
+#include "xaifBooster/system/inc/ControlFlowGraphAlgBase.hpp"
 #include "xaifBooster/system/inc/ControlFlowGraph.hpp"
+#include "xaifBooster/algorithms/ControlFlowReversal/inc/ControlFlowGraphAlgFactory.hpp"
 #include "xaifBooster/algorithms/ControlFlowReversal/inc/ControlFlowGraphAlg.hpp"
 
 namespace xaifBoosterControlFlowReversal {  
