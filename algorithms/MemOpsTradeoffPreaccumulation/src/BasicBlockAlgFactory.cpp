2ea4a8d164159e40d7101cdcd33164df79187eb2
m_utkej <m_utkej@wally.mynet.test>
2004-02-16 18:29:03 -0600

    bk-changeset-1.30.1.17 m_utkej@wally.mynet.test|ChangeSet|20040217002903|61592 ChangeSet   1.30.1.17 04/02/16 18:29:03 m_utkej@wally.mynet.test +9 -0   mv all factories to src dir and select appropriate factories via another mechanism (see previous changeset on the main branch)
      forgot to do it for MemOpsTradeoffs
    
      algorithms/MemOpsTradeoffPreaccumulation/test/t.cpp
        1.3 04/02/16 18:29:03 m_utkej@wally.mynet.test +2 -0
        mv all factories to src dir and select appropriate factories via another mechanism (see previous changeset on the main branch)
    
      algorithms/MemOpsTradeoffPreaccumulation/test/Makefile
        1.2 04/02/16 18:29:03 m_utkej@wally.mynet.test +2 -37
        mv all factories to src dir and select appropriate factories via another mechanism (see previous changeset on the main branch)
    
      algorithms/MemOpsTradeoffPreaccumulation/src/Makefile
        1.2 04/02/16 18:29:03 m_utkej@wally.mynet.test +0 -2
        mv factories to src dir
    
      algorithms/MemOpsTradeoffPreaccumulation/src/BasicBlockAlgFactory.cpp
        1.3 04/02/16 18:29:03 m_utkej@wally.mynet.test +4 -5
        mv all factories to src dir and select appropriate factories via another mechanism (see previous changeset on the main branch)
    
      algorithms/MemOpsTradeoffPreaccumulation/src/AlgFactoryManager.cpp
        1.1 04/02/16 18:26:42 m_utkej@wally.mynet.test +36 -0
    
      algorithms/MemOpsTradeoffPreaccumulation/inc/BasicBlockAlgFactory.hpp
        1.1 04/02/16 18:26:42 m_utkej@wally.mynet.test +13 -0
    
      algorithms/MemOpsTradeoffPreaccumulation/src/AlgFactoryManager.cpp
        1.0 04/02/16 18:26:42 m_utkej@wally.mynet.test +0 -0
        BitKeeper file /m_home/m_utkej/Argonne/bk/xaifBooster_current/xaifBooster/algorithms/MemOpsTradeoffPreaccumulation/src/AlgFactoryManager.cpp
    
      algorithms/MemOpsTradeoffPreaccumulation/inc/BasicBlockAlgFactory.hpp
        1.0 04/02/16 18:26:42 m_utkej@wally.mynet.test +0 -0
        BitKeeper file /m_home/m_utkej/Argonne/bk/xaifBooster_current/xaifBooster/algorithms/MemOpsTradeoffPreaccumulation/inc/BasicBlockAlgFactory.hpp
    
      algorithms/MemOpsTradeoffPreaccumulation/inc/AlgFactoryManager.hpp
        1.1 04/02/16 18:26:41 m_utkej@wally.mynet.test +30 -0
    
      algorithms/MemOpsTradeoffPreaccumulation/inc/AlgFactoryManager.hpp
        1.0 04/02/16 18:26:41 m_utkej@wally.mynet.test +0 -0
        BitKeeper file /m_home/m_utkej/Argonne/bk/xaifBooster_current/xaifBooster/algorithms/MemOpsTradeoffPreaccumulation/inc/AlgFactoryManager.hpp
    
      BitKeeper/deleted/.del-Makefile~66956fcf4f3525ff
        1.2 04/02/16 18:03:27 m_utkej@wally.mynet.test +0 -0
        Delete: algorithms/MemOpsTradeoffPreaccumulation/src/srcFactories/Makefile
    
      BitKeeper/deleted/.del-.keepme~8a5a4d5d53ccde08
        1.2 04/02/16 18:03:16 m_utkej@wally.mynet.test +0 -0
        Delete: algorithms/MemOpsTradeoffPreaccumulation/src/srcFactories/obj/.keepme
    
      algorithms/MemOpsTradeoffPreaccumulation/src/BasicBlockAlgFactory.cpp
        1.2 04/02/16 18:02:54 m_utkej@wally.mynet.test +0 -0
        Rename: algorithms/MemOpsTradeoffPreaccumulation/src/srcFactories/BasicBlockAlgFactory.cpp -> algorithms/MemOpsTradeoffPreaccumulation/src/BasicBlockAlgFactory.cpp

---
+++ algorithms/MemOpsTradeoffPreaccumulation/src/BasicBlockAlgFactory.cpp
@@ -0,0 +1,10 @@
+#include "xaifBooster/system/inc/BasicBlockAlgBase.hpp"
+#include "xaifBooster/system/inc/BasicBlock.hpp"
+#include "xaifBooster/algorithms/MemOpsTradeoffPreaccumulation/inc/BasicBlockAlgFactory.hpp"
+#include "xaifBooster/algorithms/MemOpsTradeoffPreaccumulation/inc/BasicBlockAlg.hpp"
+
+namespace xaifBoosterMemOpsTradeoffPreaccumulation {  
+
+  ALG_FACTORY_DEF_MACRO(BasicBlock,xaifBooster::BasicBlockAlgBase,BasicBlockAlg);
+
+} // end of namespace 
