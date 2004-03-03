97c6d6d6116aa037ec141796c2be8b7fbd8e9b4d
m_utkej <m_utkej@wally.mynet.test>
2004-03-03 14:15:57 -0600

    bk-changeset-1.75.1.2 m_utkej@wally.mynet.test|ChangeSet|20040303201557|15267 ChangeSet   1.75.1.2 04/03/03 14:15:57 m_utkej@wally.mynet.test +1 -0   fix script output
      testRoundTrip/pickAlgorithm.bash
        1.2 04/03/03 14:15:55 m_utkej@wally.mynet.test +2 -0
        fix output

--- testRoundTrip/pickAlgorithm.bash
+++ testRoundTrip/pickAlgorithm.bash
@@ -6,6 +6,7 @@ do
   echo $count : $i
   let count=count+1
 done 
+echo -n "pick a number: "
 read answer
 count=1
 for i in `echo ../algorithms/*/test/t`
@@ -17,4 +18,5 @@ do
   fi
   let count=count+1
 done
+echo "Error: $answer is not a valid pick from the list"
 exit -1
