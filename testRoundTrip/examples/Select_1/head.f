01bcde1884f2e7d0db86ff46f5728fee8a68b242
naumann <naumann@float177.mcs.anl-external.org>
2004-03-18 08:47:10 -0600

    bk-changeset-1.86 naumann@float177.mcs.anl-external.org|ChangeSet|20040318144710|46180 ChangeSet   1.86 04/03/18 08:47:10 naumann@float177.mcs.anl-external.org +3 -0   select case example
      testRoundTrip/examples/Select_1/head.f
        1.3 04/03/18 08:47:10 naumann@float177.mcs.anl-external.org +9 -2
        select case example
    
      testRoundTrip/examples/Select_1/refOutput/.keep
        1.1 04/03/18 08:38:01 naumann@float177.mcs.anl-external.org +0 -0
    
      testRoundTrip/examples/Select_1/refOutput/.keep
        1.0 04/03/18 08:38:01 naumann@float177.mcs.anl-external.org +0 -0
        BitKeeper file /home/naumann/bk/xaifBooster_CFGREV/xaifBooster/testRoundTrip/examples/Select_1/refOutput/.keep
    
      testRoundTrip/examples/Select_1/params.conf
        1.2 04/03/18 08:37:33 naumann@float177.mcs.anl-external.org +0 -0
        bk cp ../IfStatement_1/params.conf ./params.conf
    
      testRoundTrip/examples/Select_1/head.f
        1.2 04/03/18 08:37:17 naumann@float177.mcs.anl-external.org +0 -0
        bk cp ../IfStatement_1/head.f ./head.f
    
      testRoundTrip/examples/Select_1/params.conf
        1.1 04/02/20 16:07:02 m_utkej@wally.mynet.test +3 -0
    
      testRoundTrip/examples/Select_1/head.f
        1.1 04/02/20 16:07:02 m_utkej@wally.mynet.test +11 -0
    
      testRoundTrip/examples/Select_1/params.conf
        1.0 04/02/20 16:07:02 m_utkej@wally.mynet.test +0 -0
        BitKeeper file /m_home/m_utkej/Argonne/bk/xaifBooster_CFR/xaifBooster/testRoundTrip/examples/IfStatement_1/params.conf
    
      testRoundTrip/examples/Select_1/head.f
        1.0 04/02/20 16:07:02 m_utkej@wally.mynet.test +0 -0
        BitKeeper file /m_home/m_utkej/Argonne/bk/xaifBooster_CFR/xaifBooster/testRoundTrip/examples/IfStatement_1/head.f

---
+++ testRoundTrip/examples/Select_1/head.f
@@ -0,0 +1,18 @@
+	subroutine head(x,y)
+	  double precision, dimension(1), intent(in) :: x
+	  double precision, dimension(1), intent(out) :: y
+          integer i
+
+          i=1
+          y(1)=2. 
+          select case (i) 
+          case (1)
+            y(1)=x(1)
+          case (2)
+            y(1)=x(1)
+          case default
+            y(1)=x(1)
+          end select
+          y(1)=y(1)*y(1)
+
+	end subroutine
