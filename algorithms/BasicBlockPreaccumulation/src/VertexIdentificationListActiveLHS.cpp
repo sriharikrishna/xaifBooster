faca38d54f31b84ce99e0b811556be66cd5eb419
m_utkej <utkej1@gmail.com>
2004-03-24 23:01:38 -0600

    bk-changeset-1.93 m_utkej@wally.mynet.test|ChangeSet|20040325050138|19004 ChangeSet   1.93 04/03/24 23:01:38 m_utkej@wally.mynet.test +3 -0   fix removing in statementid list
      algorithms/BasicBlockPreaccumulation/src/VertexIdentificationListActiveLHS.cpp
        1.7 04/03/24 23:01:37 m_utkej@wally.mynet.test +27 -0
        fix removing in statementid list
    
      algorithms/BasicBlockPreaccumulation/inc/VertexIdentificationListActiveLHS.hpp
        1.7 04/03/24 23:01:37 m_utkej@wally.mynet.test +2 -0
        fix removing in statementid list
    
      algorithms/BasicBlockPreaccumulation/inc/VertexIdentificationListActive.hpp
        1.7 04/03/24 23:01:37 m_utkej@wally.mynet.test +1 -1
        fix removing in statementid list

--- algorithms/BasicBlockPreaccumulation/src/VertexIdentificationListActiveLHS.cpp
+++ algorithms/BasicBlockPreaccumulation/src/VertexIdentificationListActiveLHS.cpp
@@ -30,6 +30,11 @@ namespace xaifBoosterBasicBlockPreaccumulation {
 	  if (theResult.myStatementId==theItem.myStatementId) 
 	    return IdentificationResult(UNIQUELY_IDENTIFIED,theItem.myPrivateLinearizedComputationalGraphVertex_p);
 	} // end for
+	// we are in trouble 
+	THROW_LOGICEXCEPTION_MACRO("VertexIdentificationListActiveLHS::canIdentify: cannot match statement for "
+				   << theResult.myStatementId 
+				   << " in " 
+				   << debug().c_str());
       } // end if
       else if (theResult.myAnswer==DuUdMap::UNIQUE_OUTSIDE
 	       ||
@@ -58,6 +63,28 @@ namespace xaifBoosterBasicBlockPreaccumulation {
     myAliasMapKeyList.push_back(&(theVariable.getAliasMapKey()));
     // this is a shortcut for the current duud numbering
     myStatementIdList.push_back(theStatementId);
+  } 
+
+  void VertexIdentificationListActiveLHS::removeIfIdentifiable(const Variable& theVariable) { 
+    IdentificationResult idResult(canIdentify(theVariable));
+    while(idResult.getAnswer()!=NOT_IDENTIFIED) { 
+      ListItemPList::iterator aListIterator=myList.begin();
+      AliasMap::AliasMapKeyList::iterator aKeyListIterator=myAliasMapKeyList.begin();
+      DuUdMap::StatementIdList::iterator aStatementIdListIterator=myStatementIdList.begin();
+      for (;
+	   aListIterator!=myList.end(); 
+	   ++aListIterator,
+	     ++aKeyListIterator,
+	     ++aStatementIdListIterator) { 
+	if (dynamic_cast<ListItem&>(**aListIterator).myPrivateLinearizedComputationalGraphVertex_p==idResult.getVertexP()) { 
+	  myAliasMapKeyList.erase(aKeyListIterator);
+	  myList.erase(aListIterator);
+	  myStatementIdList.erase(aStatementIdListIterator);
+	  break;
+	} // end if 
+      } // end for
+      idResult=canIdentify(theVariable);
+    } // end while 
   } 
 
   std::string VertexIdentificationListActiveLHS::ListItem::debug() const { 
