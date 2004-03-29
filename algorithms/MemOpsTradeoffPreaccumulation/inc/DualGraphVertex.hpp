e6bdf93f3b7b8d5fab080fe28ea8711cd10c4f1b
lyonsam <utkej1@gmail.com>
2004-03-29 17:27:08 -0600

    bk-changeset-1.92.1.5 lyonsam@lyons.mcs.anl.gov|ChangeSet|20040329232708|32845 ChangeSet   1.92.1.5 04/03/29 17:27:08 lyonsam@lyons.mcs.anl.gov +7 -0   commented, added files
      algorithms/MemOpsTradeoffPreaccumulation/src/DualGraph.cpp
        1.6 04/03/29 17:27:07 lyonsam@lyons.mcs.anl.gov +16 -2
        added tmp couts for error checking
    
      algorithms/MemOpsTradeoffPreaccumulation/src/BasicBlockAlg.cpp
        1.13 04/03/29 17:27:07 lyonsam@lyons.mcs.anl.gov +6 -7
        changed comments
    
      algorithms/MemOpsTradeoffPreaccumulation/inc/DualGraphVertex.hpp
        1.1 04/03/29 17:26:22 lyonsam@lyons.mcs.anl.gov +68 -0
    
      algorithms/MemOpsTradeoffPreaccumulation/inc/DualGraphVertex.hpp
        1.0 04/03/29 17:26:22 lyonsam@lyons.mcs.anl.gov +0 -0
        BitKeeper file /home/lyonsam/Argonne/xaifBooster/algorithms/MemOpsTradeoffPreaccumulation/inc/DualGraphVertex.hpp
    
      algorithms/MemOpsTradeoffPreaccumulation/inc/DualGraphPath.hpp
        1.1 04/03/29 17:26:21 lyonsam@lyons.mcs.anl.gov +39 -0
    
      algorithms/MemOpsTradeoffPreaccumulation/inc/DualGraphEdge.hpp
        1.1 04/03/29 17:26:21 lyonsam@lyons.mcs.anl.gov +32 -0
    
      algorithms/MemOpsTradeoffPreaccumulation/inc/DualGraphPath.hpp
        1.0 04/03/29 17:26:21 lyonsam@lyons.mcs.anl.gov +0 -0
        BitKeeper file /home/lyonsam/Argonne/xaifBooster/algorithms/MemOpsTradeoffPreaccumulation/inc/DualGraphPath.hpp
    
      algorithms/MemOpsTradeoffPreaccumulation/inc/DualGraphEdge.hpp
        1.0 04/03/29 17:26:21 lyonsam@lyons.mcs.anl.gov +0 -0
        BitKeeper file /home/lyonsam/Argonne/xaifBooster/algorithms/MemOpsTradeoffPreaccumulation/inc/DualGraphEdge.hpp
    
      algorithms/MemOpsTradeoffPreaccumulation/src/DualGraphVertex.cpp
        1.1 04/03/29 17:26:14 lyonsam@lyons.mcs.anl.gov +43 -0
    
      algorithms/MemOpsTradeoffPreaccumulation/src/DualGraphPath.cpp
        1.1 04/03/29 17:26:14 lyonsam@lyons.mcs.anl.gov +17 -0
    
      algorithms/MemOpsTradeoffPreaccumulation/src/DualGraphVertex.cpp
        1.0 04/03/29 17:26:14 lyonsam@lyons.mcs.anl.gov +0 -0
        BitKeeper file /home/lyonsam/Argonne/xaifBooster/algorithms/MemOpsTradeoffPreaccumulation/src/DualGraphVertex.cpp
    
      algorithms/MemOpsTradeoffPreaccumulation/src/DualGraphPath.cpp
        1.0 04/03/29 17:26:14 lyonsam@lyons.mcs.anl.gov +0 -0
        BitKeeper file /home/lyonsam/Argonne/xaifBooster/algorithms/MemOpsTradeoffPreaccumulation/src/DualGraphPath.cpp

---
+++ algorithms/MemOpsTradeoffPreaccumulation/inc/DualGraphVertex.hpp
@@ -0,0 +1,68 @@
+/********************************************************************************************************
+ * Andrew Lyons, MCS Division, Argonne National Laboratory
+ * Under the supervision of Paul Hovland and Uwe Naumann
+ * DualGraphVertex.hpp          Last Updated: 03/16/04 16:50
+ */
+
+#ifndef _DUALGRAPHVERTEX_INCLUDE_
+#define _DUALGRAPHVERTEX_INCLUDE_
+
+#include "xaifBooster/boostWrapper/inc/Vertex.hpp"
+#include "xaifBooster/algorithms/CrossCountryInterface/inc/LinearizedComputationalGraph.hpp"
+#include "xaifBooster/algorithms/MemOpsTradeoffPreaccumulation/inc/JacobianAccumulationExpressionCopy.hpp"
+
+using namespace xaifBoosterCrossCountryInterface;
+
+namespace MemOpsTradeoffPreaccumulation {
+ 
+  class DualGraphVertex : public Vertex {
+  
+  public:
+    /*
+     * constructor sets the jacobianref to null, so that when the destructor executes, jacobianref will
+     * only be deleted if it has been set in the first place.
+     */
+    DualGraphVertex():jacobianRef(NULL) {};
+
+    ~DualGraphVertex(){
+      if(jacobianRef){
+	delete jacobianRef;
+      }
+    };
+
+    enum DualVertexRefType { TO_ORIGINAL_EDGE,
+			     TO_ASSUMED_INEDGE,
+			     TO_ASSUMED_OUTEDGE,
+			     TO_INTERNAL_EXPRESSION};
+
+    /**
+     * these functions set either the jacobianref, assumed ref or original ref pointers.
+     * these functions also set mycopyreferencetype to the corrresponding type.
+     */
+    void setOriginalRef(const LinearizedComputationalGraphEdge& theOriginalEdge);
+    void setAssumedInEdgeRef(const LinearizedComputationalGraphVertex& theAssumedInEdgeTarget);
+    void setAssumedOutEdgeRef(const LinearizedComputationalGraphVertex& theAssumedOutEdgeSource);
+    void setJacobianRef(MemOpsTradeoffPreaccumulation::JacobianAccumulationExpressionCopy* theJacobianExpression_pt);
+
+    /**
+     * these functions return the original ref, the jacobian ref, an assumed ref, or the ref type respectively.
+     */
+    const LinearizedComputationalGraphEdge& getOriginalRef() const;
+    const LinearizedComputationalGraphVertex& getAssumedRef() const;
+    MemOpsTradeoffPreaccumulation::JacobianAccumulationExpressionCopy& getJacobianRef() const;
+
+    DualVertexRefType getRefType() const;
+
+  private:
+    
+    const LinearizedComputationalGraphEdge* originalRef;
+    const LinearizedComputationalGraphVertex* assumedRef;
+    MemOpsTradeoffPreaccumulation::JacobianAccumulationExpressionCopy* jacobianRef;
+
+    DualVertexRefType myDualReferenceType;
+
+  }; // end of class
+ 
+} //end of namespace MemOpsTradeoffPreaccumulation
+                                                                     
+#endif
---
+++ algorithms/MemOpsTradeoffPreaccumulation/src/DualGraphVertex.cpp
@@ -0,0 +1,43 @@
+#include "xaifBooster/algorithms/MemOpsTradeoffPreaccumulation/inc/DualGraphVertex.hpp"
+
+using namespace xaifBoosterCrossCountryInterface;
+
+namespace MemOpsTradeoffPreaccumulation {
+
+  void DualGraphVertex::setOriginalRef(const LinearizedComputationalGraphEdge& theOriginalEdge){
+    originalRef = &theOriginalEdge;
+    myDualReferenceType = TO_ORIGINAL_EDGE;
+  }
+
+  void DualGraphVertex::setAssumedInEdgeRef(const LinearizedComputationalGraphVertex& theAssumedInEdgeTarget){
+    assumedRef = &theAssumedInEdgeTarget;
+    myDualReferenceType = TO_ASSUMED_INEDGE;
+  }
+
+  void DualGraphVertex::setAssumedOutEdgeRef(const LinearizedComputationalGraphVertex& theAssumedOutEdgeSource){
+    assumedRef = &theAssumedOutEdgeSource;
+    myDualReferenceType = TO_ASSUMED_OUTEDGE;
+  }
+
+  void DualGraphVertex::setJacobianRef(MemOpsTradeoffPreaccumulation::JacobianAccumulationExpressionCopy* theJacobianExpression_pt){
+    jacobianRef = theJacobianExpression_pt;
+    myDualReferenceType = TO_INTERNAL_EXPRESSION;
+  }
+
+  const LinearizedComputationalGraphEdge& DualGraphVertex::getOriginalRef() const {
+    return *originalRef;
+  }
+  
+  const LinearizedComputationalGraphVertex& DualGraphVertex::getAssumedRef() const {
+    return *assumedRef;
+  }
+
+  DualGraphVertex::DualVertexRefType DualGraphVertex::getRefType() const {
+    return myDualReferenceType;
+  }
+
+  MemOpsTradeoffPreaccumulation::JacobianAccumulationExpressionCopy& DualGraphVertex::getJacobianRef() const {
+    return *jacobianRef;
+  }
+
+} //end namespace
