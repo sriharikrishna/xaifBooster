761aa3f03c41436a8a9fa8ffbcabdfd49523cde2
lyonsam <lyonsam@lyons.mcs.anl.gov>
2004-04-08 14:19:19 -0600

    bk-changeset-1.104.1.1 lyonsam@lyons.mcs.anl.gov|ChangeSet|20040408201919|03292 ChangeSet   1.104.1.1 04/04/08 15:19:19 lyonsam@lyons.mcs.anl.gov +8 -0   ...
      algorithms/MemOpsTradeoffPreaccumulation/src/LinearizedComputationalGraphCopy.cpp
        1.5 04/04/08 15:19:18 lyonsam@lyons.mcs.anl.gov +28 -13
        ...
    
      algorithms/MemOpsTradeoffPreaccumulation/src/FaceElim.cpp
        1.4 04/04/08 15:19:18 lyonsam@lyons.mcs.anl.gov +121 -7
        ...
    
      algorithms/MemOpsTradeoffPreaccumulation/src/EdgeElim.cpp
        1.4 04/04/08 15:19:18 lyonsam@lyons.mcs.anl.gov +4 -2
        ...
    
      algorithms/MemOpsTradeoffPreaccumulation/src/DualGraph.cpp
        1.7 04/04/08 15:19:18 lyonsam@lyons.mcs.anl.gov +248 -60
        ...
    
      algorithms/MemOpsTradeoffPreaccumulation/src/BasicBlockAlg.cpp
        1.14.1.1 04/04/08 15:19:18 lyonsam@lyons.mcs.anl.gov +67 -145
        ...
    
      algorithms/MemOpsTradeoffPreaccumulation/inc/LinearizedComputationalGraphCopy.hpp
        1.5 04/04/08 15:19:18 lyonsam@lyons.mcs.anl.gov +3 -6
        ...
    
      algorithms/MemOpsTradeoffPreaccumulation/inc/DualGraphPath.hpp
        1.2 04/04/08 15:19:18 lyonsam@lyons.mcs.anl.gov +1 -0
        ...
    
      algorithms/MemOpsTradeoffPreaccumulation/inc/DualGraph.hpp
        1.4 04/04/08 15:19:18 lyonsam@lyons.mcs.anl.gov +6 -8
        ...

--- algorithms/MemOpsTradeoffPreaccumulation/src/DualGraph.cpp
+++ algorithms/MemOpsTradeoffPreaccumulation/src/DualGraph.cpp
@@ -4,6 +4,68 @@ using namespace MemOpsTradeoffPreaccumulation;
 
 namespace MemOpsTradeoffPreaccumulation {
 
+  DualGraph::DualGraph(const LinearizedComputationalGraph& theOriginal){
+
+    //Create Dual Graph Vertices
+      xaifBoosterCrossCountryInterface::LinearizedComputationalGraph::ConstEdgeIteratorPair deip (theOriginal.edges());
+      xaifBoosterCrossCountryInterface::LinearizedComputationalGraph::ConstEdgeIterator dei (deip.first), de_end (deip.second);
+      //iterate through edges in the original graph, and create a vertex in the dual for each one
+      for(; dei != de_end; ++dei){
+	DualGraphVertex& DV = addVertex();
+	DV.setOriginalRef(*dei);
+      }// end for original edges
+
+      xaifBoosterCrossCountryInterface::LinearizedComputationalGraph::ConstVertexIteratorPair dv (theOriginal.vertices());
+      xaifBoosterCrossCountryInterface::LinearizedComputationalGraph::ConstVertexIterator dvi (dv.first), dv_end (dv.second);
+      //iterate through vertices in the original graph, and create a vertex in the copy for each one
+      for(; dvi != dv_end; ++dvi){
+	if(theOriginal.numInEdgesOf(*dvi) == 0){
+
+	  DualGraphVertex& DV = addVertex();
+	  DV.setAssumedInEdgeRef(*dvi);
+
+	  // for each outedge of the vertex, make an edge from that outedge vertex to the assumed inedge vertex
+	  xaifBoosterCrossCountryInterface::LinearizedComputationalGraph::ConstOutEdgeIteratorPair doe (theOriginal.getOutEdgesOf(*dvi));
+	  xaifBoosterCrossCountryInterface::LinearizedComputationalGraph::ConstOutEdgeIterator doei (doe.first), doe_end (doe.second);
+	  for(; doei != doe_end; ++doei){
+	    addEdge(DV, getDualVertex(*doei));
+	  }// end for outedges
+	}// end if minimal
+	if(theOriginal.numOutEdgesOf(*dvi) == 0){
+
+	  DualGraphVertex& DV = addVertex();
+	  DV.setAssumedOutEdgeRef(*dvi);
+
+	  // for each inedge of the vertex, make an edge from the inedge to the assumed inedge vertex
+	  xaifBoosterCrossCountryInterface::LinearizedComputationalGraph::ConstInEdgeIteratorPair die (theOriginal.getInEdgesOf(*dvi));
+	  xaifBoosterCrossCountryInterface::LinearizedComputationalGraph::ConstInEdgeIterator diei (die.first), die_end (die.second);
+	  for(; diei != die_end; ++diei){
+	    addEdge(getDualVertex(*diei), DV);
+	  }// end for inedges
+	}// end if maximal
+      }// end for original vertices
+
+      //generate edges in dual graph
+      xaifBoosterCrossCountryInterface::LinearizedComputationalGraph::ConstVertexIteratorPair dv2 (theOriginal.vertices());
+      xaifBoosterCrossCountryInterface::LinearizedComputationalGraph::ConstVertexIterator dv2i (dv2.first), dv2_end (dv2.second);
+      for(; dv2i != dv2_end; ++dv2i){
+
+	xaifBoosterCrossCountryInterface::LinearizedComputationalGraph::ConstInEdgeIteratorPair die2 (theOriginal.getInEdgesOf(*dv2i));
+	xaifBoosterCrossCountryInterface::LinearizedComputationalGraph::ConstInEdgeIterator die2i (die2.first), die2_end (die2.second);
+	for(; die2i != die2_end; ++die2i){
+
+	  xaifBoosterCrossCountryInterface::LinearizedComputationalGraph::ConstOutEdgeIteratorPair doe2 (theOriginal.getOutEdgesOf(*dv2i));
+	  xaifBoosterCrossCountryInterface::LinearizedComputationalGraph::ConstOutEdgeIterator doe2i (doe2.first), doe2_end (doe2.second);
+	  for(; doe2i != doe2_end; ++doe2i){
+
+	    addEdge(getDualVertex(*die2i),getDualVertex(*doe2i));
+
+	  }// end for outedges
+	}// end for inedges
+      }// end for original vertices
+
+  }// end constructor
+
   DualGraph::~DualGraph() {
     clearPathList();
   }
@@ -33,55 +95,61 @@ namespace MemOpsTradeoffPreaccumulation {
     for(; dvi != dv_end; ++dvi){
       if(numInEdgesOf(*dvi) == 0){
 	DualGraphPath* thenewpath_p = new DualGraphPath;
-	
-	std::cout << "independent found, new path created" << std::endl;
-
+	((*thenewpath_p).myPath).push_back(&*dvi);
+	(*thenewpath_p).pathNum = 0;
+	myPathList.push_front(thenewpath_p);
 	if(numOutEdgesOf(*dvi) == 0){
 	  (*thenewpath_p).setComplete();
-
-	  std::cout << "independent has no outedges, has been set complete" << std::endl;
-
 	}// end if
 	else{
-
-	  std::cout << "independent has outedges, about to have it copied" << std::endl;
-
-	  copyPath(*thenewpath_p);
-
-	  std::cout << "independent has been copied" << std::endl;
-
+	  copyPath(thenewpath_p);
 	}// end else
       }// end if
     }// end for
 
-    std::cout << "Initial paths constructed" << std::endl;
-
     DualGraph::PathList::iterator pathi;
     for(pathi = myPathList.begin(); pathi != myPathList.end(); pathi++){
-      if(!(**pathi).isComplete()){ 
+      while(!(**pathi).isComplete()){
+
 	//iterate through successors of the last vertex in the path
         DualGraph::OutEdgeIteratorPair doe (getOutEdgesOf(*((**pathi).myPath).back()));
 	DualGraph::OutEdgeIterator doei (doe.first), doe_end (doe.second);
+	unsigned int i = 0;
 	for(; doei != doe_end; ++doei){
 	  //check to see if there is already a path from the vertex to the successor
-	  if(!checkSequence((*((**pathi).myPath).back()), getTargetOf(*doei))){
+	  if((**pathi).pathNum == i){
 	    break;
 	  }// end if
+	  i++;
 	}// end for outedges
-	if(doei != doe_end){
-	  THROW_LOGICEXCEPTION_MACRO("Error: No untraveled successor found");
+	if(doei == doe_end){
+	  THROW_LOGICEXCEPTION_MACRO("Error: No match between pathnum and out edge num could be found");
 	}// end if
 	((**pathi).myPath).push_back(&getTargetOf(*doei));
+	(**pathi).pathNum = 0;
 	if(numOutEdgesOf(getTargetOf(*doei)) == 0) {
 	  (**pathi).setComplete();
 	}// end if
 	else{
-	  copyPath(**pathi);
+	  copyPath(*pathi);
 	}// end else
 	pathi = myPathList.begin();
-      }// end if not complete
+      }// end while not complete
     }// end for all paths
+
   }// end populate path list
+
+  void DualGraph::copyPath(DualGraphPath* thepath_p) {
+
+    unsigned int i;
+    for(i = 1; i < numOutEdgesOf(*((*thepath_p).myPath).back()); i++){      
+       DualGraphPath* thenewpath_p= new DualGraphPath;
+       (*thenewpath_p).myPath = (*thepath_p).myPath;
+       (*thenewpath_p).pathNum = i;
+       myPathList.push_front(thenewpath_p);
+    }// end for
+
+  }// end copyPath
 
   void DualGraph::clearPathList() {
 
@@ -92,61 +160,181 @@ namespace MemOpsTradeoffPreaccumulation {
     
     myPathList.clear();
 
-  }// end clear path list
+  }// end clear path list 
 
-  const DualGraph::PathList& DualGraph::getPathList() const { 
-    return myPathList;
-  } 
+  DualGraph::FacePointerList DualGraph::populateElimList() {
 
-  void DualGraph::copyPath(const DualGraphPath& thePath) {
+    //iterate over all faces in the dual graph
+    EdgeIteratorPair deip (edges());
+    EdgeIterator dei (deip.first), de_end (deip.second);
+    for(; dei != de_end; ++dei){
 
-    unsigned int i;
-    for(i = 1; i < numOutEdgesOf(*(thePath.myPath).back()); i++){
-       DualGraphPath* thenewpath_p= new DualGraphPath;
-       (*thenewpath_p).myPath = thePath.myPath;
-    }// end for
+      //if the face is an intermediate and has no path conflicts
+      if((numInEdgesOf(getSourceOf(*dei))*numOutEdgesOf(getTargetOf(*dei)) > 0) && (isFinal(*dei))){
 
-  }// end copyPath
+	std::cout << "final intermediate edge added to elim list" << std::endl;
 
-  bool DualGraph::allComplete() const {
-    bool iscomplete = true;
-    //iterate through all paths
-    DualGraph::PathList::const_iterator ccheck;
-    for(ccheck = myPathList.begin(); ccheck != myPathList.end(); ccheck++){
-      if(!(**ccheck).isComplete()){
-	iscomplete = false;
-	break;
+	myElimList.push_back(&*dei);
       }// end if
-    }// end for all paths
-    if(myPathList.size() == 0){
-      iscomplete = false;
-    }// end if
-    return iscomplete;
-  }
-  
-  unsigned int DualGraph::checkSequence(DualGraphVertex& theFirst, DualGraphVertex& theSecond) {
+      else{
+	
+	std::cout << "edge not added to elim list" << std::endl;
 
-    unsigned int numSequences = 0;
+      }// end else
+
+    }// end for edges
+
+    return myElimList;
+  }// end populateElimlist
+
+  bool DualGraph::isFinal(DualGraphEdge& theFace) const {
+
+    std::cout << "inside isFinal.  source = " << &getSourceOf(theFace) << " target = " << &getTargetOf(theFace) << std::endl;
+
+    VertexPointerList vertexReach;
+
     //spath iterates through each path in the list
-    DualGraph::PathList::iterator spath;
-    //svertex iterates through each vertex in the path
+    DualGraph::PathList::const_iterator spath;
+    //svertex iterates through each vertex in the path, svertexr iterates in reverse order
     DualGraphPath::Path::iterator svertex;
-    
-    //for each path in the list
+    DualGraphPath::Path::reverse_iterator svertexr;
+    DualGraph::VertexPointerList::iterator reachi;
+
+    //check for alternate paths from source
+    //built list of reachable from target
     for(spath = myPathList.begin(); spath != myPathList.end(); spath++){
+
+      std::cout << "entering new path" << std::endl;
+
       //for each vertex in the path
       for(svertex = ((**spath).myPath).begin(); svertex != ((**spath).myPath).end(); svertex++){
-	if(*svertex == &theFirst){
+
+	std::cout << "trying new vertex" << std::endl;
+
+	//if the path contains the target vertex
+	if(*svertex == &getTargetOf(theFace)){
+
+	  std::cout << "target found" << std::endl;
+
+	  //iterate through the remaining vertices and add them to vertexReach
 	  svertex++;
-	  if(*svertex == &theSecond){
-	    numSequences++;
-	  }
-	  break;
-	}// end if
+	  for(; svertex != ((**spath).myPath).end(); svertex++){
+
+	    std::cout << "adding subsequent vertex to reachlist" << std::endl;
+
+	    vertexReach.push_back(*svertex);
+	  }//end for remaining vertices
+	  svertex--;
+	}// end if source is in path
       }// end for vertices
     }// end for paths
 
-    return numSequences;
-  }// end checkSequence
+    std::cout << "reachable from target built" << std::endl;
+
+    //go through list again, find paths that contain source
+    for(spath = myPathList.begin(); spath != myPathList.end(); spath++){
+
+      std::cout << "checking new path" << std::endl;
+
+      //for each vertex in the path
+      for(svertex = ((**spath).myPath).begin(); svertex != ((**spath).myPath).end(); svertex++){
+	if(*svertex == &getSourceOf(theFace)){
+	  svertex++;
+	  //if the path contains the face
+	  if(*svertex == &getTargetOf(theFace)){
+
+	    std::cout << "path contains the face" << std::endl;
+
+	    //leave this path
+	    break;
+	  }
+	  //if path doesnt have source->target it doesnt have face
+	  else{
+	    //for each remaining vertex in the path
+	    for(; svertex != ((**spath).myPath).end(); svertex++){
+	      
+	      for(reachi = vertexReach.begin(); reachi != vertexReach.end(); reachi++){
+		if(*svertex == *reachi){
+
+		  std::cout << "reachable from target conflict found, isfinal returned false" << std::endl;
+
+		  return false;
+		
+		}// end if reachi = svertex
+	      }// end for all vertexReach
+	    }//end for remaining vertices
+
+	    svertex--;
+	    
+	  }// end else
+	}//end if path contains source
+      }//end for each vertex in the path
+    }//end for all paths
+
+    vertexReach.clear();
+
+      
+    std::cout << "found no conflicts, building reachable to source" << std::endl;
+
+    //check for alternate paths to target
+    //built list of reachable to source
+    for(spath = myPathList.begin(); spath != myPathList.end(); spath++){
+      //for each vertex in the path
+      for(svertexr = ((**spath).myPath).rbegin(); svertexr != ((**spath).myPath).rend(); svertexr++){
+	//if the path contains the source vertex
+	if(*svertexr == &getSourceOf(theFace)){
+	  //iterate through the previous vertices and add them to vertexReach
+	  svertexr++;
+	  for(; svertexr != ((**spath).myPath).rend(); svertexr++){
+	    vertexReach.push_back(*svertexr);
+	  }//end for remaining vertices
+	  svertexr--;
+	}// end if source is in path
+      }// end for vertices
+    }// end for paths
+
+    std::cout << "reachable to source built" << std::endl;
+
+    //go through list again, find paths that contain target
+    for(spath = myPathList.begin(); spath != myPathList.end(); spath++){
+      //for each vertex in the path
+      for(svertexr = ((**spath).myPath).rbegin(); svertexr != ((**spath).myPath).rend(); svertexr++){
+	//if the path contains the target
+	if(*svertexr == &getTargetOf(theFace)){
+	  svertexr++;
+	  //if the path contains the face
+	  if(*svertexr == &getSourceOf(theFace)){
+	    //leave this path
+	    break;
+	  }
+	  //if path doesnt have source->target it doesnt have face
+	  else{
+	    //for each remaining vertex in the path
+	    for(; svertexr != ((**spath).myPath).rend(); svertexr++){
+
+	      for(reachi = vertexReach.begin(); reachi != vertexReach.end(); reachi++){
+		if(*svertex == *reachi){
+
+		  std::cout << "reachable to source conflict found, isfinal returned false" << std::endl;
+
+		  return false;
+		}// end if reachi = svertex
+	      }// end for all vertexReach
+	    }//end for remaining vertices
+
+	    svertexr--;
+
+	  }// end else
+	}//end if path contains source
+      }//end for each vertex in the path
+    }//end for all paths
+
+
+    return true;
+  }// end isFinal
+
+  void DualGraph::clearElimList() {
+    myElimList.clear();
+  }
 
 }// end namespace MemOpsTradeoffPreaccumulation
