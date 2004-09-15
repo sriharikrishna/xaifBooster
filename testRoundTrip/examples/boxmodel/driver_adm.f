4a9f96d794ed1120ba17981cbf6662f00a374921
au016na <au016na@nb-naumann.rz.rwth-aachen.de>
2004-09-15 01:55:00 -0600

    bk-changeset-1.114.17.16 au016na@nb-naumann.rz.rwth-aachen.de|ChangeSet|20040915075500|46373 ChangeSet   1.114.17.16 04/09/15 09:55:00 au016na@nb-naumann.rz.rwth-aachen.de +2 -0   ...
      testRoundTrip/examples/boxmodel/head.f
        1.5 04/09/15 09:55:00 au016na@nb-naumann.rz.rwth-aachen.de +0 -81
        ...
    
      testRoundTrip/examples/boxmodel/driver_adm.f
        1.3 04/09/15 09:55:00 au016na@nb-naumann.rz.rwth-aachen.de +27 -24
        ...

--- testRoundTrip/examples/boxmodel/driver_adm.f
+++ testRoundTrip/examples/boxmodel/driver_adm.f
@@ -1,48 +1,51 @@
-       program boxmodel_tl_driver
+       program boxmodel_adm_driver
 
          use active_module
+         use OpenAD_rev
+         use OpenAD_tape
+         use all_globals_mod
          implicit none
 
       integer, parameter :: kdim=3
       integer i,j
-
-!     from box_ini_fields
-
-      COMMON /fi_state_r/ TOLD, TNOW, TNEW, SOLD, SNOW, SNEW
-      type(active) :: TOLD(1 : 3)
-      type(active) :: TNOW(1 : 3)
-      type(active) :: TNEW(1 : 3)
-      type(active) :: SOLD(1 : 3)
-      type(active) :: SNOW(1 : 3)
-      type(active) :: SNEW(1 : 3)
-
-      common /fi_controls_r/ xx
-      type(active) :: xx(1:6)
 
       external box_model_body
 
 !
 ! ACTS code
 !
-        print*, "Jacobian (computed by TLM, output transposed)"
+        print*, "Jacobian (computed by split adjoint)"
         do i=1,2*kdim
           call box_ini_params
           call box_ini_fields
-          do j=1,2*kdim
-            call zero_deriv(xx(j))
-          enddo
-          xx(i)%d=1.D0
+          call taping_mode()
           call box_model_body
           do j=1,kdim
-            write(*,'(F6.4,"  ")',ADVANCE='NO') tnew(j)%d
-          end do
-          do j=1,kdim
-            write(*,'(F6.4,"  ")',ADVANCE='NO') snew(j)%d
+            call zero_deriv(tnew(j))
+            call zero_deriv(snew(j))
+          enddo
+          if (i>kdim) then
+            do j=1,kdim
+              if (j==i+kdim) then
+                snew(j)%d=1
+              end if
+            end do
+          else
+            do j=1,kdim
+              if (j==i) then
+                tnew(j)%d=1
+              end if
+            end do
+          end if
+          call adjoint_mode()
+          call box_model_body
+          do j=1,2*kdim
+            write(*,'(F6.4,"  ")',ADVANCE='NO') xx(j)%d
           end do
           write(*,*)
         end do
 
-	end program boxmodel_tl_driver
+	end program boxmodel_adm_driver
 
 
 
