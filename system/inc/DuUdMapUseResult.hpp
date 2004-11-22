90529eeda249965cb7a66fc59a6c99f0ca4cf9cd
utkej <utkej1@gmail.com>
2004-11-22 13:19:08 -0600

    bk-changeset-1.129 utkej@tina.mynet.test|ChangeSet|20041122191908|26942 ChangeSet   1.129 04/11/22 13:19:08 utkej@tina.mynet.test +4 -0   missing files
      system/src/DuUdMapEntry.cpp
        1.1 04/11/22 13:18:00 utkej@tina.mynet.test +117 -0
    
      system/src/DuUdMapDefinitionResult.cpp
        1.1 04/11/22 13:18:00 utkej@tina.mynet.test +13 -0
    
      system/src/DuUdMapEntry.cpp
        1.0 04/11/22 13:18:00 utkej@tina.mynet.test +0 -0
        BitKeeper file /sandbox/bk/xaifBooster_NewOA/xaifBooster/system/src/DuUdMapEntry.cpp
    
      system/src/DuUdMapDefinitionResult.cpp
        1.0 04/11/22 13:18:00 utkej@tina.mynet.test +0 -0
        BitKeeper file /sandbox/bk/xaifBooster_NewOA/xaifBooster/system/src/DuUdMapDefinitionResult.cpp
    
      system/inc/DuUdMapUseResult.hpp
        1.1 04/11/22 13:16:58 utkej@tina.mynet.test +15 -0
    
      system/inc/DuUdMapDefinitionResult.hpp
        1.1 04/11/22 13:16:58 utkej@tina.mynet.test +37 -0
    
      system/inc/DuUdMapUseResult.hpp
        1.0 04/11/22 13:16:58 utkej@tina.mynet.test +0 -0
        BitKeeper file /sandbox/bk/xaifBooster_NewOA/xaifBooster/system/inc/DuUdMapUseResult.hpp
    
      system/inc/DuUdMapDefinitionResult.hpp
        1.0 04/11/22 13:16:58 utkej@tina.mynet.test +0 -0
        BitKeeper file /sandbox/bk/xaifBooster_NewOA/xaifBooster/system/inc/DuUdMapDefinitionResult.hpp

---
+++ system/inc/DuUdMapUseResult.hpp
@@ -0,0 +1,15 @@
+#ifndef _DUUDMAPUSERESULT_INCLUDE_
+#define _DUUDMAPUSERESULT_INCLUDE_
+
+namespace xaifBooster { 
+
+  /** 
+   * for now just identical to DuUdMapDefinitionResult
+   */
+  class DuUdMapUseResult : public DuUdMapDefinitionResult {
+
+  };
+
+} // end of namespace 
+                                                                     
+#endif
