0a33e5891959853a9e9aa51a7cd4ac1f3dfdfaab
lyonsam <lyonsam@ogd.arbor>
2005-06-09 17:17:44 -0600

    bk-changeset-1.170.1.9 lyonsam@ogd.arbor|ChangeSet|20050609231744|54937 ChangeSet   1.170.1.9 05/06/09 18:17:44 lyonsam@ogd.arbor +3 -0   I...removed arrays
      BitKeeper/etc/logging_ok
        1.11 05/06/09 18:17:18 lyonsam@ogd.arbor +1 -0
        Logging to logging@openlogging.org accepted
    
      testRoundTrip/examples/RoehFlux_MemOps/head.f
        1.1.1.2 05/06/09 18:17:00 lyonsam@ogd.arbor +54 -37
        removed arrays
    
      testRoundTrip/examples/RoehFlux_MemOps/driver_tlm.f90
        1.2 05/06/09 18:17:00 lyonsam@ogd.arbor +95 -91
        removed arrays

--- testRoundTrip/examples/RoehFlux_MemOps/driver_tlm.f90
+++ testRoundTrip/examples/RoehFlux_MemOps/driver_tlm.f90
@@ -6,17 +6,17 @@
           external ad_roehf5
 
           ! inputs
-          type(active) nrm(3), priml(5), primr(5) 
+          type(active) nrm1, nrm2, nrm3, priml1, priml2, priml3, priml4, priml5, primr1, primr2, primr3, primr4, primr5 
           type(active) gamma, gm1, gm1inv, nlefix
           type(active) lefix, mcheps
 
-          type(active)  nrm_ph(3), priml_ph(5), primr_ph(5)
+          type(active) nrm_ph1, nrm_ph2, nrm_ph3, priml_ph1, priml_ph2, priml_ph3, priml_ph4, priml_ph5, primr_ph1, primr_ph2, primr_ph3, primr_ph4, primr_ph5
           type(active) gamma_ph, gm1_ph, gm1inv_ph, nlefix_ph
           type(active) lefix_ph, mcheps_ph
 
           ! outputs
-          type(active) flux(5)
-          type(active) flux_ph(5)
+          type(active) flux1, flux2, flux3, flux4, flux5
+          type(active) flux_ph1, flux_ph2, flux_ph3, flux_ph4, flux_ph5
           
 	  double precision, dimension(19) :: x0
           real :: h=0.00001
@@ -29,19 +29,19 @@
 
           open(2,file='tmpOutput/dd.out')
           write(2,*) "DD"
-          nrm(1)%v=x0(1)
-          nrm(2)%v=x0(2)
-          nrm(3)%v=x0(3)
-          priml(1)%v=x0(4)
-          priml(2)%v=x0(5)
-          priml(3)%v=x0(6)
-          priml(4)%v=x0(7)
-          priml(5)%v=x0(8)
-          primr(1)%v=x0(9)
-          primr(2)%v=x0(10)
-          primr(3)%v=x0(11)
-          primr(4)%v=x0(12)
-          primr(5)%v=x0(13)
+          nrm1%v=x0(1)
+          nrm2%v=x0(2)
+          nrm3%v=x0(3)
+          priml1%v=x0(4)
+          priml2%v=x0(5)
+          priml3%v=x0(6)
+          priml4%v=x0(7)
+          priml5%v=x0(8)
+          primr1%v=x0(9)
+          primr2%v=x0(10)
+          primr3%v=x0(11)
+          primr4%v=x0(12)
+          primr5%v=x0(13)
           gamma%v=x0(14) 
           gm1%v=x0(15) 
           gm1inv%v=x0(16) 
@@ -49,22 +49,22 @@
           lefix%v=x0(18)
           mcheps%v=x0(19)
 
-          call ad_roehf5 (nrm, priml, primr, gamma, gm1, gm1inv, nlefix, lefix, mcheps, flux)
+          call ad_roehf5 (nrm1, nrm2, nrm3, priml1, priml2, priml3, priml4, priml5, primr1, primr2, primr3, primr4, primr5, gamma, gm1, gm1inv, nlefix, lefix, mcheps, flux1, flux2, flux3, flux4, flux5)
           
 	  do i=1,n   
-              nrm_ph(1)%v=x0(1)
-              nrm_ph(2)%v=x0(2)
-              nrm_ph(3)%v=x0(3)
-              priml_ph(1)%v=x0(4)
-              priml_ph(2)%v=x0(5)
-              priml_ph(3)%v=x0(6)
-              priml_ph(4)%v=x0(7)
-              priml_ph(5)%v=x0(8)
-              primr_ph(1)%v=x0(9)
-              primr_ph(2)%v=x0(10)
-              primr_ph(3)%v=x0(11)
-              primr_ph(4)%v=x0(12)
-              primr_ph(5)%v=x0(13)
+              nrm_ph1%v=x0(1)
+              nrm_ph2%v=x0(2)
+              nrm_ph3%v=x0(3)
+              priml_ph1%v=x0(4)
+              priml_ph2%v=x0(5)
+              priml_ph3%v=x0(6)
+              priml_ph4%v=x0(7)
+              priml_ph5%v=x0(8)
+              primr_ph1%v=x0(9)
+              primr_ph2%v=x0(10)
+              primr_ph3%v=x0(11)
+              primr_ph4%v=x0(12)
+              primr_ph5%v=x0(13)
               gamma_ph%v=x0(14) 
               gm1_ph%v=x0(15) 
               gm1inv_ph%v=x0(16) 
@@ -73,48 +73,50 @@
               mcheps_ph%v=x0(19)
 
 
-              if (i==1) nrm_ph(1)%v=nrm_ph(1)%v+h
-              if (i==2) nrm_ph(2)%v=nrm_ph(2)%v+h
-              if (i==3) nrm_ph(3)%v=nrm_ph(3)%v+h
-              if (i==4) priml_ph(1)%v=priml_ph(1)%v+h
-              if (i==5) priml_ph(2)%v=priml_ph(2)%v+h
-              if (i==6) priml_ph(3)%v=priml_ph(3)%v+h
-              if (i==7) priml_ph(4)%v=priml_ph(4)%v+h
-              if (i==8) priml_ph(5)%v=priml_ph(5)%v+h
-              if (i==9) primr_ph(1)%v=primr_ph(1)%v+h
-              if (i==10) primr_ph(2)%v=primr_ph(2)%v+h
-              if (i==11) primr_ph(3)%v=primr_ph(3)%v+h
-              if (i==12) primr_ph(4)%v=primr_ph(4)%v+h
-              if (i==13) primr_ph(5)%v=primr_ph(5)%v+h
+              if (i==1) nrm_ph1%v=nrm_ph1%v+h
+              if (i==2) nrm_ph2%v=nrm_ph2%v+h
+              if (i==3) nrm_ph3%v=nrm_ph3%v+h
+              if (i==4) priml_ph1%v=priml_ph1%v+h
+              if (i==5) priml_ph2%v=priml_ph2%v+h
+              if (i==6) priml_ph3%v=priml_ph3%v+h
+              if (i==7) priml_ph4%v=priml_ph4%v+h
+              if (i==8) priml_ph5%v=priml_ph5%v+h
+              if (i==9) primr_ph1%v=primr_ph1%v+h
+              if (i==10) primr_ph2%v=primr_ph2%v+h
+              if (i==11) primr_ph3%v=primr_ph3%v+h
+              if (i==12) primr_ph4%v=primr_ph4%v+h
+              if (i==13) primr_ph5%v=primr_ph5%v+h
               if (i==14) gamma_ph%v=gamma_ph%v+h
               if (i==15) gm1_ph%v=gm1_ph%v+h
               if (i==16) gm1inv_ph%v=gm1inv_ph%v+h
               if (i==17) nlefix_ph%v=nlefix_ph%v+h
               if (i==18) lefix_ph%v=lefix_ph%v+h
               if (i==19) mcheps_ph%v=mcheps_ph%v+h
-              call ad_roehf5 (nrm_ph, priml_ph, primr_ph, gamma_ph, gm1_ph, gm1inv_ph, nlefix_ph, lefix_ph, mcheps_ph, flux_ph)
-              do k=1,m
-                 write(2,'(A,I3,A,I3,A,EN26.16E3)') "F(",k,",",i,")=",(flux_ph(k)%v-flux(k)%v)/h
-              end do
+              call ad_roehf5 (nrm_ph1, nrm_ph2, nrm_ph3, priml_ph1, priml_ph2, priml_ph3, priml_ph4, priml_ph5, primr_ph1, primr_ph2, primr_ph3, primr_ph4, primr_ph5, gamma_ph, gm1_ph, gm1inv_ph, nlefix_ph, lefix_ph, mcheps_ph, flux_ph1, flux_ph2, flux_ph3, flux_ph4, flux_ph5)
+              write(2,*) "F(",1,",",i,")=",(flux_ph1%v-flux1%v)/h
+              write(2,*) "F(",2,",",i,")=",(flux_ph2%v-flux2%v)/h
+              write(2,*) "F(",3,",",i,")=",(flux_ph3%v-flux3%v)/h
+              write(2,*) "F(",4,",",i,")=",(flux_ph4%v-flux4%v)/h
+              write(2,*) "F(",5,",",i,")=",(flux_ph5%v-flux5%v)/h
           end do
           close(2)
 
           open(2,file='tmpOutput/ad.out')
           write(2,*) "AD"
 	  do i=1,n   
-              nrm(1)%v=x0(1)
-              nrm(2)%v=x0(2)
-              nrm(3)%v=x0(3)
-              priml(1)%v=x0(4)
-              priml(2)%v=x0(5)
-              priml(3)%v=x0(6)
-              priml(4)%v=x0(7)
-              priml(5)%v=x0(8)
-              primr(1)%v=x0(9)
-              primr(2)%v=x0(10)
-              primr(3)%v=x0(11)
-              primr(4)%v=x0(12)
-              primr(5)%v=x0(13)
+              nrm1%v=x0(1)
+              nrm2%v=x0(2)
+              nrm3%v=x0(3)
+              priml1%v=x0(4)
+              priml2%v=x0(5)
+              priml3%v=x0(6)
+              priml4%v=x0(7)
+              priml5%v=x0(8)
+              primr1%v=x0(9)
+              primr2%v=x0(10)
+              primr3%v=x0(11)
+              primr4%v=x0(12)
+              primr5%v=x0(13)
               gamma%v=x0(14) 
               gm1%v=x0(15) 
               gm1inv%v=x0(16) 
@@ -122,48 +124,50 @@
               lefix%v=x0(18)
               mcheps%v=x0(19)
 
-              nrm(1)%d=0.
-              nrm(2)%d=0.
-              nrm(3)%d=0.
-              priml(1)%d=0.
-              priml(2)%d=0.
-              priml(3)%d=0.
-              priml(4)%d=0.
-              priml(5)%d=0.
-              primr(1)%d=0.
-              primr(2)%d=0.
-              primr(3)%d=0.
-              primr(4)%d=0.
-              primr(5)%d=0.
+              nrm1%d=0.
+              nrm2%d=0.
+              nrm3%d=0.
+              priml1%d=0.
+              priml2%d=0.
+              priml3%d=0.
+              priml4%d=0.
+              priml5%d=0.
+              primr1%d=0.
+              primr2%d=0.
+              primr3%d=0.
+              primr4%d=0.
+              primr5%d=0.
               gamma%d=0.
               gm1%d=0.
               gm1inv%d=0.
               nlefix%d=0.
               lefix%d=0.
               mcheps%d=0.
-                if (i==1) nrm(1)%d=1.
-                if (i==2) nrm(2)%d=1.
-                if (i==3) nrm(3)%d=1.
-                if (i==4) priml(1)%d=1.
-                if (i==5) priml(2)%d=1.
-                if (i==6) priml(3)%d=1.
-                if (i==7) priml(4)%d=1.
-                if (i==8) priml(5)%d=1.
-                if (i==9) primr(1)%d=1.
-                if (i==10) primr(2)%d=1.
-                if (i==11) primr(3)%d=1.
-                if (i==12) primr(4)%d=1.
-                if (i==13) primr(5)%d=1.
+                if (i==1) nrm1%d=1.
+                if (i==2) nrm2%d=1.
+                if (i==3) nrm3%d=1.
+                if (i==4) priml1%d=1.
+                if (i==5) priml2%d=1.
+                if (i==6) priml3%d=1.
+                if (i==7) priml4%d=1.
+                if (i==8) priml5%d=1.
+                if (i==9) primr1%d=1.
+                if (i==10) primr2%d=1.
+                if (i==11) primr3%d=1.
+                if (i==12) primr4%d=1.
+                if (i==13) primr5%d=1.
                 if (i==14) gamma%d=1.
                 if (i==15) gm1%d=1.
                 if (i==16) gm1inv%d=1.
                 if (i==17) nlefix%d=1.
                 if (i==18) lefix%d=1.
                 if (i==19) mcheps%d=1.
-            call ad_roehf5 (nrm, priml, primr, gamma, gm1, gm1inv, nlefix, lefix, mcheps, flux)
-            do k=1,m
-              write(2,'(A,I3,A,I3,A,EN26.16E3)') "F(",k,",",i,")=",flux(k)%d
-            end do
+            call ad_roehf5 (nrm1, nrm2, nrm3, priml1, priml2, priml3, priml4, priml5, primr1, primr2, primr3, primr4, primr5, gamma, gm1, gm1inv, nlefix, lefix, mcheps, flux1, flux2, flux3, flux4, flux5)
+            write(2,*) "F(",1,",",i,")=",flux1%d
+            write(2,*) "F(",2,",",i,")=",flux2%d
+            write(2,*) "F(",3,",",i,")=",flux3%d
+            write(2,*) "F(",4,",",i,")=",flux4%d
+            write(2,*) "F(",5,",",i,")=",flux5%d
           end do
           close(2)
 
