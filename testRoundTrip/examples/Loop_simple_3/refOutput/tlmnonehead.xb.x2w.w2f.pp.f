4d36412fcbbed28d99ec6e1528735e29a93e6acf
m_utkej <utkej1@gmail.com>
2006-08-07 14:52:13 -0500

    missed update for reference output copied from another example

--- testRoundTrip/examples/Loop_simple_3/refOutput/tlmnonehead.xb.x2w.w2f.pp.f
+++ testRoundTrip/examples/Loop_simple_3/refOutput/tlmnonehead.xb.x2w.w2f.pp.f
@@ -1,5 +1,5 @@
 C ***********************************************************
-C Fortran file translated from WHIRL Fri Jul 21 15:13:03 2006
+C Fortran file translated from WHIRL Mon Aug  7 14:51:14 2006
 C ***********************************************************
 C ***********************************************************
 
@@ -13,25 +13,31 @@ C     **** Global Variables & Derived Type Definitions ****
 C
       REAL(w2f__8) OpenAD_Symbol_0
       REAL(w2f__8) OpenAD_Symbol_1
+      REAL(w2f__8) OpenAD_Symbol_10
+      type(active) :: OpenAD_Symbol_11
+      REAL(w2f__8) OpenAD_Symbol_12
+      type(active) :: OpenAD_Symbol_13
+      type(active) :: OpenAD_Symbol_14
       REAL(w2f__8) OpenAD_Symbol_2
-      type(active) :: OpenAD_Symbol_3
+      REAL(w2f__8) OpenAD_Symbol_3
       REAL(w2f__8) OpenAD_Symbol_4
       type(active) :: OpenAD_Symbol_5
       REAL(w2f__8) OpenAD_Symbol_6
       type(active) :: OpenAD_Symbol_7
-      type(active) :: OpenAD_Symbol_8
+      REAL(w2f__8) OpenAD_Symbol_8
+      type(active) :: OpenAD_Symbol_9
 C
 C     **** Parameters and Result ****
 C
       type(active) :: X(1 : 1)
+      INTENT(IN)  X
       type(active) :: Y(1 : 1)
+      INTENT(OUT)  Y
 C
 C     **** Local Variables and Functions ****
 C
       CHARACTER(10) ASTRING
       INTEGER(w2f__i4) I
-      REAL(w2f__8) OpenAD_Symbol_10
-      REAL(w2f__8) OpenAD_Symbol_9
 C
 C     **** Top Level Pragmas ****
 C
@@ -40,21 +46,10 @@ C$OPENAD DEPENDENT(Y)
 C
 C     **** Statements ****
 C
-C     $OpenAD$ BEGIN REPLACEMENT 1
 C$OPENAD XXX Template ad_template.f
       Y(1)%v = X(1)%v
-      ASTRING = 'blah'
-C$OPENAD XXX Simple loop
-      DO I = 1, 3, 1
-        IF(ASTRING .EQ. 'blah') THEN
-          Y(1)%v = (X(1)%v*Y(1)%v)
-        ENDIF
-      END DO
-      RETURN
-C     $OpenAD$ END REPLACEMENT
-C     $OpenAD$ BEGIN REPLACEMENT 2
-C$OPENAD XXX Template ad_template.f
-      Y(1)%v = X(1)%v
+      CALL setderiv(OpenAD_Symbol_5,X(1))
+      CALL setderiv(Y(1),OpenAD_Symbol_5)
       ASTRING = 'blah'
 C$OPENAD XXX Simple loop
       DO I = 1, 3, 1
@@ -63,92 +58,26 @@ C$OPENAD XXX Simple loop
           OpenAD_Symbol_0 = Y(1)%v
           OpenAD_Symbol_1 = X(1)%v
           Y(1)%v = OpenAD_Symbol_2
-          OpenAD_Symbol_4 = OpenAD_Symbol_0
-          OpenAD_Symbol_6 = OpenAD_Symbol_1
-C         $OpenAD$ INLINE push(subst)
-          CALL push(OpenAD_Symbol_4)
-C         $OpenAD$ INLINE push(subst)
-          CALL push(OpenAD_Symbol_6)
+          OpenAD_Symbol_6 = OpenAD_Symbol_0
+          OpenAD_Symbol_8 = OpenAD_Symbol_1
+          CALL setderiv(OpenAD_Symbol_9,Y(1))
+          CALL setderiv(OpenAD_Symbol_7,X(1))
+          CALL sax(OpenAD_Symbol_6,OpenAD_Symbol_7,Y(1))
+          CALL saxpy(OpenAD_Symbol_8,OpenAD_Symbol_9,Y(1))
+        ENDIF
+        IF(ASTRING .EQ. 'bloh') THEN
+          Y(1)%v = (Y(1)%v-X(1)%v)
+          OpenAD_Symbol_3 = 1_w2f__i8
+          OpenAD_Symbol_4 = (-1_w2f__i8)
+          OpenAD_Symbol_10 = OpenAD_Symbol_3
+          OpenAD_Symbol_12 = OpenAD_Symbol_4
+          CALL setderiv(OpenAD_Symbol_13,X(1))
+          CALL setderiv(OpenAD_Symbol_11,Y(1))
+          CALL sax(OpenAD_Symbol_10,OpenAD_Symbol_11,Y(1))
+          CALL saxpy(OpenAD_Symbol_12,OpenAD_Symbol_13,Y(1))
         ENDIF
       END DO
-C     $OpenAD$ INLINE push_s(subst)
-      CALL push_s(ASTRING)
+      CALL setderiv(OpenAD_Symbol_14,Y(1))
+      CALL setderiv(Y(1),OpenAD_Symbol_14)
       RETURN
-C     $OpenAD$ END REPLACEMENT
-C     $OpenAD$ BEGIN REPLACEMENT 3
-C     $OpenAD$ INLINE CondIncZeroDeriv(subst,subst)
-      CALL CondIncZeroDeriv(Y(1),OpenAD_Symbol_8)
-C     $OpenAD$ INLINE IncDeriv(subst,subst)
-      CALL IncDeriv(OpenAD_Symbol_8,Y(1))
-C     $OpenAD$ INLINE ZeroDeriv(subst)
-      CALL ZeroDeriv(OpenAD_Symbol_8)
-C     $OpenAD$ INLINE pop_s(subst)
-      CALL pop_s(ASTRING)
-      I = 1 + 1 *((3 - 1) / 1)
-      DO WHILE(I .GE. 1)
-        IF(ASTRING .EQ. 'blah') THEN
-C         $OpenAD$ INLINE Pop(subst)
-          CALL Pop(OpenAD_Symbol_9)
-C         $OpenAD$ INLINE Saxpy(subst,subst,subst)
-          CALL Saxpy(OpenAD_Symbol_9,Y(1),OpenAD_Symbol_7)
-C         $OpenAD$ INLINE Pop(subst)
-          CALL Pop(OpenAD_Symbol_10)
-C         $OpenAD$ INLINE Saxpy(subst,subst,subst)
-          CALL Saxpy(OpenAD_Symbol_10,Y(1),OpenAD_Symbol_5)
-C         $OpenAD$ INLINE ZeroDeriv(subst)
-          CALL ZeroDeriv(Y(1))
-C         $OpenAD$ INLINE IncDeriv(subst,subst)
-          CALL IncDeriv(OpenAD_Symbol_5,X(1))
-C         $OpenAD$ INLINE ZeroDeriv(subst)
-          CALL ZeroDeriv(OpenAD_Symbol_5)
-C         $OpenAD$ INLINE IncDeriv(subst,subst)
-          CALL IncDeriv(OpenAD_Symbol_7,Y(1))
-C         $OpenAD$ INLINE ZeroDeriv(subst)
-          CALL ZeroDeriv(OpenAD_Symbol_7)
-        ENDIF
-        I = I - 1
-      END DO
-C     $OpenAD$ INLINE CondIncZeroDeriv(subst,subst)
-      CALL CondIncZeroDeriv(Y(1),OpenAD_Symbol_3)
-C     $OpenAD$ INLINE IncDeriv(subst,subst)
-      CALL IncDeriv(OpenAD_Symbol_3,X(1))
-C     $OpenAD$ INLINE ZeroDeriv(subst)
-      CALL ZeroDeriv(OpenAD_Symbol_3)
-C     $OpenAD$ END REPLACEMENT
-C     $OpenAD$ BEGIN REPLACEMENT 4
-C     $OpenAD$ INLINE cp_arg_store_real_vector_a(subst)
-      CALL cp_arg_store_real_vector_a(X)
-C     $OpenAD$ INLINE cp_arg_store_real_vector_a(subst)
-      CALL cp_arg_store_real_vector_a(Y)
-C     $OpenAD$ END REPLACEMENT
-C     $OpenAD$ BEGIN REPLACEMENT 5
-C     $OpenAD$ INLINE cp_res_store_real_vector_a(subst)
-      CALL cp_res_store_real_vector_a(Y)
-C     $OpenAD$ END REPLACEMENT
-C     $OpenAD$ BEGIN REPLACEMENT 6
-C     $OpenAD$ INLINE cp_arg_restore_real_vector_a(subst)
-      CALL cp_arg_restore_real_vector_a(Y)
-C     $OpenAD$ INLINE cp_arg_restore_real_vector_a(subst)
-      CALL cp_arg_restore_real_vector_a(X)
-C     $OpenAD$ END REPLACEMENT
-C     $OpenAD$ BEGIN REPLACEMENT 7
-C     $OpenAD$ INLINE cp_res_restore_real_vector_a(subst)
-      CALL cp_res_restore_real_vector_a(Y)
-C     $OpenAD$ END REPLACEMENT
-C     $OpenAD$ BEGIN REPLACEMENT 8
-C     $OpenAD$ INLINE cp_tsarg_store_real_vector_a(subst)
-      CALL cp_tsarg_store_real_vector_a(Y)
-C     $OpenAD$ INLINE cp_arg_store_real_vector_a(subst)
-      CALL cp_arg_store_real_vector_a(X)
-C     $OpenAD$ INLINE cp_arg_store_real_vector_a(subst)
-      CALL cp_arg_store_real_vector_a(Y)
-C     $OpenAD$ END REPLACEMENT
-C     $OpenAD$ BEGIN REPLACEMENT 9
-C     $OpenAD$ INLINE cp_tsarg_restore_real_vector_a(subst)
-      CALL cp_tsarg_restore_real_vector_a(Y)
-C     $OpenAD$ INLINE cp_tsarg_restore_real_vector_a(subst)
-      CALL cp_tsarg_restore_real_vector_a(X)
-C     $OpenAD$ INLINE cp_arg_restore_real_vector_a(subst)
-      CALL cp_arg_restore_real_vector_a(Y)
-C     $OpenAD$ END REPLACEMENT
       END SUBROUTINE
--- testRoundTrip/examples/Loop_simple_3/refOutput/tlmnonehead_sf.xb.x2w.w2f.f
+++ testRoundTrip/examples/Loop_simple_3/refOutput/tlmnonehead_sf.xb.x2w.w2f.f
@@ -1,5 +1,5 @@
 C ***********************************************************
-C Fortran file translated from WHIRL Fri Jul 21 15:13:03 2006
+C Fortran file translated from WHIRL Mon Aug  7 14:51:14 2006
 C ***********************************************************
 C ***********************************************************
 
@@ -20,25 +20,31 @@ C     **** Global Variables & Derived Type Definitions ****
 C
       REAL(w2f__8) OpenAD_Symbol_0
       REAL(w2f__8) OpenAD_Symbol_1
+      REAL(w2f__8) OpenAD_Symbol_10
+      TYPE (OpenADTy_active) OpenAD_Symbol_11
+      REAL(w2f__8) OpenAD_Symbol_12
+      TYPE (OpenADTy_active) OpenAD_Symbol_13
+      TYPE (OpenADTy_active) OpenAD_Symbol_14
       REAL(w2f__8) OpenAD_Symbol_2
-      TYPE (OpenADTy_active) OpenAD_Symbol_3
+      REAL(w2f__8) OpenAD_Symbol_3
       REAL(w2f__8) OpenAD_Symbol_4
       TYPE (OpenADTy_active) OpenAD_Symbol_5
       REAL(w2f__8) OpenAD_Symbol_6
       TYPE (OpenADTy_active) OpenAD_Symbol_7
-      TYPE (OpenADTy_active) OpenAD_Symbol_8
+      REAL(w2f__8) OpenAD_Symbol_8
+      TYPE (OpenADTy_active) OpenAD_Symbol_9
 C
 C     **** Parameters and Result ****
 C
       TYPE (OpenADTy_active) X(1 : 1)
+      INTENT(IN)  X
       TYPE (OpenADTy_active) Y(1 : 1)
+      INTENT(OUT)  Y
 C
 C     **** Local Variables and Functions ****
 C
       CHARACTER(10) ASTRING
       INTEGER(w2f__i4) I
-      REAL(w2f__8) OpenAD_Symbol_10
-      REAL(w2f__8) OpenAD_Symbol_9
 C
 C     **** Top Level Pragmas ****
 C
@@ -47,21 +53,10 @@ C$OPENAD DEPENDENT(Y)
 C
 C     **** Statements ****
 C
-C     $OpenAD$ BEGIN REPLACEMENT 1
 C$OPENAD XXX Template ad_template.f
       __value__(Y(1)) = __value__(X(1))
-      ASTRING = 'blah'
-C$OPENAD XXX Simple loop
-      DO I = 1, 3, 1
-        IF(ASTRING .EQ. 'blah') THEN
-          __value__(Y(1)) = (__value__(X(1)) * __value__(Y(1)))
-        ENDIF
-      END DO
-      RETURN
-C     $OpenAD$ END REPLACEMENT
-C     $OpenAD$ BEGIN REPLACEMENT 2
-C$OPENAD XXX Template ad_template.f
-      __value__(Y(1)) = __value__(X(1))
+      CALL setderiv(__deriv__(OpenAD_Symbol_5), __deriv__(X(1)))
+      CALL setderiv(__deriv__(Y(1)), __deriv__(OpenAD_Symbol_5))
       ASTRING = 'blah'
 C$OPENAD XXX Simple loop
       DO I = 1, 3, 1
@@ -70,96 +65,30 @@ C$OPENAD XXX Simple loop
           OpenAD_Symbol_0 = __value__(Y(1))
           OpenAD_Symbol_1 = __value__(X(1))
           __value__(Y(1)) = OpenAD_Symbol_2
-          OpenAD_Symbol_4 = OpenAD_Symbol_0
-          OpenAD_Symbol_6 = OpenAD_Symbol_1
-C         $OpenAD$ INLINE push(subst)
-          CALL push(OpenAD_Symbol_4)
-C         $OpenAD$ INLINE push(subst)
-          CALL push(OpenAD_Symbol_6)
+          OpenAD_Symbol_6 = OpenAD_Symbol_0
+          OpenAD_Symbol_8 = OpenAD_Symbol_1
+          CALL setderiv(__deriv__(OpenAD_Symbol_9), __deriv__(Y(1)))
+          CALL setderiv(__deriv__(OpenAD_Symbol_7), __deriv__(X(1)))
+          CALL sax(OpenAD_Symbol_6, __deriv__(OpenAD_Symbol_7),
+     >  __deriv__(Y(1)))
+          CALL saxpy(OpenAD_Symbol_8, __deriv__(OpenAD_Symbol_9),
+     >  __deriv__(Y(1)))
+        ENDIF
+        IF(ASTRING .EQ. 'bloh') THEN
+          __value__(Y(1)) = (__value__(Y(1)) - __value__(X(1)))
+          OpenAD_Symbol_3 = 1_w2f__i8
+          OpenAD_Symbol_4 = (-1_w2f__i8)
+          OpenAD_Symbol_10 = OpenAD_Symbol_3
+          OpenAD_Symbol_12 = OpenAD_Symbol_4
+          CALL setderiv(__deriv__(OpenAD_Symbol_13), __deriv__(X(1)))
+          CALL setderiv(__deriv__(OpenAD_Symbol_11), __deriv__(Y(1)))
+          CALL sax(OpenAD_Symbol_10, __deriv__(OpenAD_Symbol_11),
+     >  __deriv__(Y(1)))
+          CALL saxpy(OpenAD_Symbol_12, __deriv__(OpenAD_Symbol_13),
+     >  __deriv__(Y(1)))
         ENDIF
       END DO
-C     $OpenAD$ INLINE push_s(subst)
-      CALL push_s(ASTRING)
+      CALL setderiv(__deriv__(OpenAD_Symbol_14), __deriv__(Y(1)))
+      CALL setderiv(__deriv__(Y(1)), __deriv__(OpenAD_Symbol_14))
       RETURN
-C     $OpenAD$ END REPLACEMENT
-C     $OpenAD$ BEGIN REPLACEMENT 3
-C     $OpenAD$ INLINE CondIncZeroDeriv(subst,subst)
-      CALL CondIncZeroDeriv(__deriv__(Y(1)), __deriv__(OpenAD_Symbol_8)
-     > )
-C     $OpenAD$ INLINE IncDeriv(subst,subst)
-      CALL IncDeriv(__deriv__(OpenAD_Symbol_8), __deriv__(Y(1)))
-C     $OpenAD$ INLINE ZeroDeriv(subst)
-      CALL ZeroDeriv(__deriv__(OpenAD_Symbol_8))
-C     $OpenAD$ INLINE pop_s(subst)
-      CALL pop_s(ASTRING)
-      I = 1 + 1 *((3 - 1) / 1)
-      DO WHILE(I .GE. 1)
-        IF(ASTRING .EQ. 'blah') THEN
-C         $OpenAD$ INLINE Pop(subst)
-          CALL Pop(OpenAD_Symbol_9)
-C         $OpenAD$ INLINE Saxpy(subst,subst,subst)
-          CALL Saxpy(OpenAD_Symbol_9, __deriv__(Y(1)), __deriv__(
-     > OpenAD_Symbol_7))
-C         $OpenAD$ INLINE Pop(subst)
-          CALL Pop(OpenAD_Symbol_10)
-C         $OpenAD$ INLINE Saxpy(subst,subst,subst)
-          CALL Saxpy(OpenAD_Symbol_10, __deriv__(Y(1)), __deriv__(
-     > OpenAD_Symbol_5))
-C         $OpenAD$ INLINE ZeroDeriv(subst)
-          CALL ZeroDeriv(__deriv__(Y(1)))
-C         $OpenAD$ INLINE IncDeriv(subst,subst)
-          CALL IncDeriv(__deriv__(OpenAD_Symbol_5), __deriv__(X(1)))
-C         $OpenAD$ INLINE ZeroDeriv(subst)
-          CALL ZeroDeriv(__deriv__(OpenAD_Symbol_5))
-C         $OpenAD$ INLINE IncDeriv(subst,subst)
-          CALL IncDeriv(__deriv__(OpenAD_Symbol_7), __deriv__(Y(1)))
-C         $OpenAD$ INLINE ZeroDeriv(subst)
-          CALL ZeroDeriv(__deriv__(OpenAD_Symbol_7))
-        ENDIF
-        I = I - 1
-      END DO
-C     $OpenAD$ INLINE CondIncZeroDeriv(subst,subst)
-      CALL CondIncZeroDeriv(__deriv__(Y(1)), __deriv__(OpenAD_Symbol_3)
-     > )
-C     $OpenAD$ INLINE IncDeriv(subst,subst)
-      CALL IncDeriv(__deriv__(OpenAD_Symbol_3), __deriv__(X(1)))
-C     $OpenAD$ INLINE ZeroDeriv(subst)
-      CALL ZeroDeriv(__deriv__(OpenAD_Symbol_3))
-C     $OpenAD$ END REPLACEMENT
-C     $OpenAD$ BEGIN REPLACEMENT 4
-C     $OpenAD$ INLINE cp_arg_store_real_vector_a(subst)
-      CALL cp_arg_store_real_vector_a(__deriv__(X))
-C     $OpenAD$ INLINE cp_arg_store_real_vector_a(subst)
-      CALL cp_arg_store_real_vector_a(__deriv__(Y))
-C     $OpenAD$ END REPLACEMENT
-C     $OpenAD$ BEGIN REPLACEMENT 5
-C     $OpenAD$ INLINE cp_res_store_real_vector_a(subst)
-      CALL cp_res_store_real_vector_a(__deriv__(Y))
-C     $OpenAD$ END REPLACEMENT
-C     $OpenAD$ BEGIN REPLACEMENT 6
-C     $OpenAD$ INLINE cp_arg_restore_real_vector_a(subst)
-      CALL cp_arg_restore_real_vector_a(__deriv__(Y))
-C     $OpenAD$ INLINE cp_arg_restore_real_vector_a(subst)
-      CALL cp_arg_restore_real_vector_a(__deriv__(X))
-C     $OpenAD$ END REPLACEMENT
-C     $OpenAD$ BEGIN REPLACEMENT 7
-C     $OpenAD$ INLINE cp_res_restore_real_vector_a(subst)
-      CALL cp_res_restore_real_vector_a(__deriv__(Y))
-C     $OpenAD$ END REPLACEMENT
-C     $OpenAD$ BEGIN REPLACEMENT 8
-C     $OpenAD$ INLINE cp_tsarg_store_real_vector_a(subst)
-      CALL cp_tsarg_store_real_vector_a(__deriv__(Y))
-C     $OpenAD$ INLINE cp_arg_store_real_vector_a(subst)
-      CALL cp_arg_store_real_vector_a(__deriv__(X))
-C     $OpenAD$ INLINE cp_arg_store_real_vector_a(subst)
-      CALL cp_arg_store_real_vector_a(__deriv__(Y))
-C     $OpenAD$ END REPLACEMENT
-C     $OpenAD$ BEGIN REPLACEMENT 9
-C     $OpenAD$ INLINE cp_tsarg_restore_real_vector_a(subst)
-      CALL cp_tsarg_restore_real_vector_a(__deriv__(Y))
-C     $OpenAD$ INLINE cp_tsarg_restore_real_vector_a(subst)
-      CALL cp_tsarg_restore_real_vector_a(__deriv__(X))
-C     $OpenAD$ INLINE cp_arg_restore_real_vector_a(subst)
-      CALL cp_arg_restore_real_vector_a(__deriv__(Y))
-C     $OpenAD$ END REPLACEMENT
       END SUBROUTINE
