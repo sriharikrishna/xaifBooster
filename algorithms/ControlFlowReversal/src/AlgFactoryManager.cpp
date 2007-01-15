078601e4d85a4423bae0a77081beaac5086d6271
m_utkej <m_utkej@utke2.mcs.anl.gov>
2007-01-15 16:32:17 -0600

    remove unnecessary dependency to Linearization from ControlFlowReversal (the inheritance pattern was also inconsistent between the algorithm classes and the quickregresssion did not work before this cleanup)

--- algorithms/ControlFlowReversal/src/AlgFactoryManager.cpp
+++ algorithms/ControlFlowReversal/src/AlgFactoryManager.cpp
@@ -116,8 +116,7 @@ namespace xaifBoosterControlFlowReversal {
   }
 
   void AlgFactoryManager::init() {
-    xaifBoosterLinearization::AlgFactoryManager::init();
-    //    xaifBooster::AlgFactoryManager::init();
+    xaifBooster::AlgFactoryManager::init();
     xaifBoosterControlFlowReversal::AlgFactoryManager::resets();
   }
 
