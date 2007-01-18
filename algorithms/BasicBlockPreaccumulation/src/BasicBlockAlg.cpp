4a1b2fb512284eb59e0a91f98c436c8ff0695e4d
ikarlin <ikarlin@shakey.mcs.anl.gov>
2007-01-17 22:08:42 -0600

    #Program no longer runs statement level for scarce algorithm

--- algorithms/BasicBlockPreaccumulation/src/BasicBlockAlg.cpp
+++ algorithms/BasicBlockPreaccumulation/src/BasicBlockAlg.cpp
@@ -522,8 +522,11 @@ namespace xaifBoosterBasicBlockPreaccumulation {
   BasicBlockAlg::algorithm_action_3() {
     DBG_MACRO(DbgGroup::CALLSTACK, "BasicBlockAlg::algorihm_action_3: invoked for "
 	      << debug().c_str());
-    if (ourPreaccumulationLevel!=PreaccumulationLevel::STATEMENT)
-      algorithm_action_3_perSequence(myFlatOn);
+    if(!useScarce)
+    {
+      if (ourPreaccumulationLevel!=PreaccumulationLevel::STATEMENT)
+        algorithm_action_3_perSequence(myFlatOn);
+    }
     if (ourPreaccumulationLevel!=PreaccumulationLevel::MAX_GRAPH)
       algorithm_action_3_perSequence(myFlatOff);
     switch(ourPreaccumulationLevel) { 
