16aa4436061af38d5f93aa6f81b10c4d273d318e
m_utkej <utkej1@gmail.com>
2007-01-29 14:36:51 -0600

    allow "all" on the command line

--- testRoundTrip/testAll.bash
+++ testRoundTrip/testAll.bash
@@ -152,7 +152,7 @@ else
   fi
 fi
 # expand single command line argument with wildcards: 
-if [ $# -eq 1 -a ! -d "examples/$1" ] 
+if [ $# -eq 1 -a ! -d "examples/$1" -a "$1" != "all" ] 
 then 
   TESTFILES=`ls -d examples/$1 | grep -v CVS | sed 's/examples\///'`
 fi
