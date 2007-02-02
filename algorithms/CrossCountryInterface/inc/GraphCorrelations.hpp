5a605da59b1838e9660af67e7e756b97ce4163b2
m_utkej <m_utkej@utke2.mcs.anl.gov>
2007-02-02 16:55:10 -0600

    some renaming etc.

--- algorithms/CrossCountryInterface/inc/GraphCorrelations.hpp
+++ algorithms/CrossCountryInterface/inc/GraphCorrelations.hpp
@@ -58,18 +58,45 @@
 
 namespace xaifBoosterCrossCountryInterface {
 
+  /** 
+   * a vertex in the remainder graph 
+   * should have a counter part in the original
+   * linearized computational graph
+   */
   struct VertexCorrelationEntry {
-    const LinearizedComputationalGraphVertex* lcgVert;
-    LinearizedComputationalGraphVertex* rv;
+    const LinearizedComputationalGraphVertex* myOriginalVertex_p;
+    LinearizedComputationalGraphVertex* myRemainderVertex_p;
+    VertexCorrelationEntry(): 
+      myOriginalVertex_p(0),
+      myRemainderVertex_p(0) {
+    };
   };
   
-  enum RemainderEdgeType {LCG_EDGE, JAE_VERT};
-  
-  union EdgeCorrelationEntry {
-    const LinearizedComputationalGraphEdge* lcgEdge;
-    JacobianAccumulationExpressionVertex* jaeVert;
-    RemainderEdgeType type;
-    LinearizedComputationalGraphEdge* re;
+  /** 
+   * an edge in the remainder graph 
+   * should reference either an edge in the original
+   * linearized computational graph
+   * or a top level vertex in an JacobianAccumulationExpression
+   */
+  struct EdgeCorrelationEntry {
+    
+    union { 
+      const LinearizedComputationalGraphEdge* myOriginalEdge_p;
+      JacobianAccumulationExpressionVertex* myJAEVertex_p;
+    } myElminationReference;
+
+    enum RemainderEdgeType_E {LCG_EDGE, 
+			      JAE_VERT,
+			      UNDEFINED};
+    /** 
+     * the discriminator for myElminationReference
+     */
+    RemainderEdgeType_E myType;
+    LinearizedComputationalGraphEdge* myRemainderGraphEdge_p;
+    EdgeCorrelationEntry() : 
+      myType(UNDEFINED),
+      myRemainderGraphEdge_p(0) {
+    };
   };
   
   typedef std::list<VertexCorrelationEntry> VertexCorrelationList;
