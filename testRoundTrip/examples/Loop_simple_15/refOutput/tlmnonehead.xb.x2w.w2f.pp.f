4271192e1445b59dc055fd9a402b2a7387cc3a55
m_utkej <utkej1@gmail.com>
2007-09-11 08:41:41 -0500

    updated examples and new reference output

---
+++ testRoundTrip/examples/Loop_simple_15/refOutput/tlmnonehead.xb.x2w.w2f.pp.f
@@ -0,0 +1,65 @@
+C ***********************************************************
+C Fortran file translated from WHIRL Thu Sep  6 23:54:41 2007
+C ***********************************************************
+C ***********************************************************
+
+
+      SUBROUTINE head(X, Y)
+      use w2f__types
+      use active_module
+      IMPLICIT NONE
+C
+C     **** Global Variables & Derived Type Definitions ****
+C
+      REAL(w2f__8) OpenAD_Symbol_0
+      REAL(w2f__8) OpenAD_Symbol_2
+      REAL(w2f__8) OpenAD_Symbol_3
+      REAL(w2f__8) OpenAD_Symbol_5
+      type(active) :: OpenAD_Symbol_6
+      REAL(w2f__8) OpenAD_Symbol_7
+      type(active) :: OpenAD_Symbol_8
+C
+C     **** Parameters and Result ****
+C
+      type(active) :: X(1 : 2)
+      type(active) :: Y(1 : 2)
+C
+C     **** Local Variables and Functions ****
+C
+      INTEGER(w2f__i4) I
+      REAL(w2f__8) S
+C
+C     **** Top Level Pragmas ****
+C
+C$OPENAD INDEPENDENT(X)
+C$OPENAD DEPENDENT(Y)
+C
+C     **** Statements ****
+C
+C$OPENAD XXX Template ad_template.f
+      I = 1
+      Y(1)%v = 0.0D00
+      Y(2)%v = 0.0D00
+      S = 1.0D00
+      CALL zero_deriv(Y(1))
+      CALL zero_deriv(Y(2))
+C$OPENAD XXX Simple loop
+      DO I = 1, 2, 1
+        IF(S .LT. 0.0D00) THEN
+          OpenAD_Symbol_2 = (X(I)%v*2.0D00)
+          OpenAD_Symbol_0 = 2.0D00
+          X(INT(I))%v = OpenAD_Symbol_2
+          OpenAD_Symbol_5 = OpenAD_Symbol_0
+          CALL setderiv(OpenAD_Symbol_6,X(I))
+          CALL sax(OpenAD_Symbol_5,OpenAD_Symbol_6,X(I))
+        ELSE
+          S = 0.0D00
+        ENDIF
+        Y(INT(I))%v = (X(I)%v+S)
+        OpenAD_Symbol_3 = 1_w2f__i8
+        OpenAD_Symbol_7 = OpenAD_Symbol_3
+        CALL setderiv(OpenAD_Symbol_8,X(I))
+        CALL sax(OpenAD_Symbol_7,OpenAD_Symbol_8,Y(I))
+      END DO
+      RETURN
+      END SUBROUTINE
