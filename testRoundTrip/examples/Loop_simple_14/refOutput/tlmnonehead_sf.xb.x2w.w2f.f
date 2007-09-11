f3fedb4ee760c258f900a228054e9dd972e25469
m_utkej <m_utkej@utke2.mcs.anl.gov>
2007-09-11 08:41:41 -0500

    updated examples and new reference output

---
+++ testRoundTrip/examples/Loop_simple_14/refOutput/tlmnonehead_sf.xb.x2w.w2f.f
@@ -0,0 +1,86 @@
+C ***********************************************************
+C Fortran file translated from WHIRL Thu Sep  6 23:54:36 2007
+C ***********************************************************
+C ***********************************************************
+
+      MODULE all_globals_mod
+      use w2f__types
+      IMPLICIT NONE
+      SAVE
+C
+C     **** Statements ****
+C
+      END MODULE
+
+      SUBROUTINE head(X, Y)
+      use w2f__types
+      IMPLICIT NONE
+C
+C     **** Global Variables & Derived Type Definitions ****
+C
+      REAL(w2f__8) OpenAD_Symbol_0
+      REAL(w2f__8) OpenAD_Symbol_10
+      TYPE (OpenADTy_active) OpenAD_Symbol_11
+      REAL(w2f__8) OpenAD_Symbol_2
+      REAL(w2f__8) OpenAD_Symbol_3
+      REAL(w2f__8) OpenAD_Symbol_4
+      TYPE (OpenADTy_active) OpenAD_Symbol_5
+      REAL(w2f__8) OpenAD_Symbol_6
+      TYPE (OpenADTy_active) OpenAD_Symbol_7
+      REAL(w2f__8) OpenAD_Symbol_8
+      TYPE (OpenADTy_active) OpenAD_Symbol_9
+C
+C     **** Parameters and Result ****
+C
+      TYPE (OpenADTy_active) X(1 : 2)
+      TYPE (OpenADTy_active) Y(1 : 2)
+C
+C     **** Local Variables and Functions ****
+C
+      INTEGER(w2f__i4) I
+      TYPE (OpenADTy_active) S
+C
+C     **** Top Level Pragmas ****
+C
+C$OPENAD INDEPENDENT(X)
+C$OPENAD DEPENDENT(Y)
+C
+C     **** Statements ****
+C
+C$OPENAD XXX Template ad_template.f
+      I = 1
+      __value__(Y(1)) = 0.0D00
+      __value__(Y(2)) = 0.0D00
+      CALL zero_deriv(__deriv__(Y(1)))
+      CALL zero_deriv(__deriv__(Y(2)))
+C$OPENAD XXX Simple loop
+      DO I = 1, 2, 1
+        __value__(S) = __value__(X(I))
+        CALL setderiv(__deriv__(OpenAD_Symbol_5), __deriv__(X(I)))
+        CALL setderiv(__deriv__(S), __deriv__(OpenAD_Symbol_5))
+        IF(__value__(S) .LT. 0.0D00) THEN
+          OpenAD_Symbol_2 = (__value__(X(I)) * 2.0D00)
+          OpenAD_Symbol_0 = 2.0D00
+          __value__(X(INT(I))) = OpenAD_Symbol_2
+          OpenAD_Symbol_6 = OpenAD_Symbol_0
+          CALL setderiv(__deriv__(OpenAD_Symbol_7), __deriv__(X(I)))
+          CALL sax(OpenAD_Symbol_6, __deriv__(OpenAD_Symbol_7),
+     >  __deriv__(X(I)))
+        ELSE
+          __value__(S) = 0.0D00
+          CALL zero_deriv(__deriv__(S))
+        ENDIF
+        __value__(Y(INT(I))) = (__value__(X(I)) + __value__(S))
+        OpenAD_Symbol_3 = 1_w2f__i8
+        OpenAD_Symbol_4 = 1_w2f__i8
+        OpenAD_Symbol_8 = OpenAD_Symbol_3
+        OpenAD_Symbol_10 = OpenAD_Symbol_4
+        CALL setderiv(__deriv__(OpenAD_Symbol_11), __deriv__(S))
+        CALL setderiv(__deriv__(OpenAD_Symbol_9), __deriv__(X(I)))
+        CALL sax(OpenAD_Symbol_8, __deriv__(OpenAD_Symbol_9), __deriv__
+     > (Y(I)))
+        CALL saxpy(OpenAD_Symbol_10, __deriv__(OpenAD_Symbol_11),
+     >  __deriv__(Y(I)))
+      END DO
+      RETURN
+      END SUBROUTINE
---
+++ testRoundTrip/examples/Loop_simple_14/refOutput/tlmnonehead_sf.xb.x2w.w2f.pp.f
@@ -0,0 +1,85 @@
+C ***********************************************************
+C Fortran file translated from WHIRL Thu Sep  6 23:54:36 2007
+C ***********************************************************
+C ***********************************************************
+
+      MODULE all_globals_mod
+      use w2f__types
+      use active_module
+      IMPLICIT NONE
+      SAVE
+C
+C     **** Statements ****
+C
+      END MODULE
+
+      SUBROUTINE head(X, Y)
+      use w2f__types
+      use active_module
+      IMPLICIT NONE
+C
+C     **** Global Variables & Derived Type Definitions ****
+C
+      REAL(w2f__8) OpenAD_Symbol_0
+      REAL(w2f__8) OpenAD_Symbol_10
+      type(active) :: OpenAD_Symbol_11
+      REAL(w2f__8) OpenAD_Symbol_2
+      REAL(w2f__8) OpenAD_Symbol_3
+      REAL(w2f__8) OpenAD_Symbol_4
+      type(active) :: OpenAD_Symbol_5
+      REAL(w2f__8) OpenAD_Symbol_6
+      type(active) :: OpenAD_Symbol_7
+      REAL(w2f__8) OpenAD_Symbol_8
+      type(active) :: OpenAD_Symbol_9
+C
+C     **** Parameters and Result ****
+C
+      type(active) :: X(1 : 2)
+      type(active) :: Y(1 : 2)
+C
+C     **** Local Variables and Functions ****
+C
+      INTEGER(w2f__i4) I
+      type(active) :: S
+C
+C     **** Top Level Pragmas ****
+C
+C$OPENAD INDEPENDENT(X)
+C$OPENAD DEPENDENT(Y)
+C
+C     **** Statements ****
+C
+C$OPENAD XXX Template ad_template.f
+      I = 1
+      Y(1)%v = 0.0D00
+      Y(2)%v = 0.0D00
+      CALL zero_deriv(Y(1))
+      CALL zero_deriv(Y(2))
+C$OPENAD XXX Simple loop
+      DO I = 1, 2, 1
+        S%v = X(I)%v
+        CALL setderiv(OpenAD_Symbol_5,X(I))
+        CALL setderiv(S,OpenAD_Symbol_5)
+        IF (S%v .LT. 0.0D00) THEN
+          OpenAD_Symbol_2 = (X(I)%v*2.0D00)
+          OpenAD_Symbol_0 = 2.0D00
+          X(INT(I))%v = OpenAD_Symbol_2
+          OpenAD_Symbol_6 = OpenAD_Symbol_0
+          CALL setderiv(OpenAD_Symbol_7,X(I))
+          CALL sax(OpenAD_Symbol_6,OpenAD_Symbol_7,X(I))
+        ELSE
+          S%v = 0.0D00
+          CALL zero_deriv(S)
+        ENDIF
+        Y(INT(I))%v = (X(I)%v+S%v)
+        OpenAD_Symbol_3 = 1_w2f__i8
+        OpenAD_Symbol_4 = 1_w2f__i8
+        OpenAD_Symbol_8 = OpenAD_Symbol_3
+        OpenAD_Symbol_10 = OpenAD_Symbol_4
+        CALL setderiv(OpenAD_Symbol_11,S)
+        CALL setderiv(OpenAD_Symbol_9,X(I))
+        CALL sax(OpenAD_Symbol_8,OpenAD_Symbol_9,Y(I))
+        CALL saxpy(OpenAD_Symbol_10,OpenAD_Symbol_11,Y(I))
+      END DO
+      RETURN
+      END SUBROUTINE
