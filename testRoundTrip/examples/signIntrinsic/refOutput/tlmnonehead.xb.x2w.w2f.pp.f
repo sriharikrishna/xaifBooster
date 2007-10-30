5c9ebc56c47e8be4f4b5637d62c005963fb2a38e
m_utkej <utkej1@gmail.com>
2007-10-30 16:11:12 -0500

    dsign test

---
+++ testRoundTrip/examples/signIntrinsic/refOutput/tlmnonehead.xb.x2w.w2f.pp.f
@@ -0,0 +1,37 @@
+
+
+      SUBROUTINE head(X, Y)
+      use w2f__types
+      use active_module
+      IMPLICIT NONE
+C
+C     **** Global Variables & Derived Type Definitions ****
+C
+      REAL(w2f__8) OpenAD_Symbol_0
+      REAL(w2f__8) OpenAD_Symbol_1
+      REAL(w2f__8) OpenAD_Symbol_3
+      REAL(w2f__8) OpenAD_Symbol_4
+      type(active) :: OpenAD_Symbol_5
+C
+C     **** Parameters and Result ****
+C
+      type(active) :: X(1 : 2)
+      type(active) :: Y(1 : 1)
+C
+C     **** Top Level Pragmas ****
+C
+C$OPENAD INDEPENDENT(X)
+C$OPENAD DEPENDENT(Y)
+C
+C     **** Statements ****
+C
+C$OPENAD XXX Template ad_template.f
+      OpenAD_Symbol_0 = (-X(2)%v)
+      OpenAD_Symbol_3 = SIGN(X(1)%v,OpenAD_Symbol_0)
+      OpenAD_Symbol_1 = (SIGN(1.0D00,X(1)%v)*SIGN(1.0D00,OpenAD_Symbol_0
+     +))
+      Y(1)%v = OpenAD_Symbol_3
+      OpenAD_Symbol_4 = OpenAD_Symbol_1
+      CALL setderiv(OpenAD_Symbol_5,X(1))
+      CALL sax(OpenAD_Symbol_4,OpenAD_Symbol_5,Y(1))
+      END SUBROUTINE
