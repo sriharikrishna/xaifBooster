0691fcf3c2d5f8a55f5081c9d3eda33f55f3c4c7
m_utkej <m_utkej@utke2.site>
2008-12-29 12:13:03 -0600

    add new container for definition results

---
+++ system/inc/FindDefinitionResult.hpp
@@ -0,0 +1,108 @@
+#ifndef _FINDDEFINITIONRESULT_INCLUDE_
+#define _FINDDEFINITIONRESULT_INCLUDE_
+// ========== begin copyright notice ==============
+// This file is part of 
+// ---------------
+// xaifBooster
+// ---------------
+// Distributed under the BSD license as follows:
+// Copyright (c) 2005, The University of Chicago
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, 
+// with or without modification, are permitted provided that the following conditions are met:
+//
+//    - Redistributions of source code must retain the above copyright notice, 
+//      this list of conditions and the following disclaimer.
+//    - Redistributions in binary form must reproduce the above copyright notice, 
+//      this list of conditions and the following disclaimer in the documentation 
+//      and/or other materials provided with the distribution.
+//    - Neither the name of The University of Chicago nor the names of its contributors 
+//      may be used to endorse or promote products derived from this software without 
+//      specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY 
+// EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES 
+// OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT 
+// SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, 
+// INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, 
+// PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT 
+// LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE 
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+// 
+// General Information:
+// xaifBooster is intended for the transformation of 
+// numerical programs represented as xml files according 
+// to the XAIF schema. It is part of the OpenAD framework. 
+// The main application is automatic 
+// differentiation, i.e. the generation of code for 
+// the computation of derivatives. 
+// The following people are the principal authors of the 
+// current version: 
+// 	Uwe Naumann
+//	Jean Utke
+// Additional contributors are: 
+//	Andrew Lyons
+//	Peter Fine
+//
+// For more details about xaifBooster and its use in OpenAD please visit:
+//   http://www.mcs.anl.gov/openad
+//
+// This work is partially supported by:
+// 	NSF-ITR grant OCE-0205590
+// ========== end copyright notice ==============
+
+
+namespace xaifBooster { 
+
+  class BasicBlockElement;
+
+
+  class Assignment;
+
+  /** 
+   * class to return a definition statement 
+   * if one was found
+   */
+  class FindDefinitionResult { 
+
+  public:
+    
+    FindDefinitionResult(const BasicBlockElement* aDefinitionStatement_p);
+
+    ~FindDefinitionResult() {};
+
+    bool foundIt();
+
+    /** 
+     * what defines a value may not 
+     * be an assignment but some other 
+     * kind of statement like a subroutine call etc.
+     */ 
+    bool foundAssignment();
+    
+    const Assignment& getAssignment();
+
+  private:
+
+    /**
+     * no def
+     */
+    FindDefinitionResult();
+
+    /**
+     * no def
+     */
+    FindDefinitionResult operator=(const FindDefinitionResult&);
+
+    /**
+     * if this is null we haven't found anything
+     */
+    const BasicBlockElement* myDefinitionStatement_p;
+
+  }; // end of class FindDefinitionResult
+
+} // end of namespace xaifBooster
+                                                                     
+#endif
