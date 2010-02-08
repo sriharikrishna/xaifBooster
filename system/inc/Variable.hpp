90d5571b6cd8184d73b4255d45ea22032ebb0c18
m_utkej <utkej1@gmail.com>
2010-02-08 08:44:16 -0600

    doxygen comments and extraneous private

--- system/inc/Variable.hpp
+++ system/inc/Variable.hpp
@@ -191,15 +191,14 @@ namespace xaifBooster {
     void setActiveUseType(ActiveUseType::ActiveUseType_E anActiveUseType); 
 
     /** 
-     * returns the first VariableVertex that is an ArrayAccess
-     * \todo this is a bit of a hack because we might 
-     * conceivably have something with more than one such vertex
+     * \return the first VariableVertex that is an ArrayAccess
+     * \todo this is a bit of a hack because we might conceivably have something with more than one such vertex
      */
     const ArrayAccess& getArrayAccess() const;
     ArrayAccess& getArrayAccess();
 
     /** 
-     * returns true if this has a VariableVertex that is an ArrayAccess
+     * \return true if this has a VariableVertex that is an ArrayAccess
      * \todo see also getArrayAccess 
      */
     bool hasArrayAccess() const;
@@ -248,8 +247,6 @@ namespace xaifBooster {
      * active type
      */
     bool myDerivFlag;
-
-  private:
 
     /** 
      * indicating how a variable is used, 
