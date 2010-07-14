f5a34b45f790cab61512c853657046a1e17d9742
utke <utke@linux-l3b2.site>
2010-07-14 13:54:01 -0500

    PARTIAL change: fix up the algorithm conifguration logic so it works with the virtual inheritance diamond pattern

--- algorithms/PushPop/driver/oadDriver.cpp
+++ algorithms/PushPop/driver/oadDriver.cpp
@@ -1,6 +1,57 @@
+// ========== begin copyright notice ==============
+// This file is part of 
+// ---------------
+// xaifBooster
+// ---------------
+// Distributed under the BSD license as follows:
+// Copyright (c) 2005, The University of Chicago
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, 
+// with or without modification, are permitted provided that the following conditions are met:
+//
+//    - Redistributions of source code must retain the above copyright notice, 
+//      this list of conditions and the following disclaimer.
+//    - Redistributions in binary form must reproduce the above copyright notice, 
+//      this list of conditions and the following disclaimer in the documentation 
+//      and/or other materials provided with the distribution.
+//    - Neither the name of The University of Chicago nor the names of its contributors 
+//      may be used to endorse or promote products derived from this software without 
+//      specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY 
+// EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES 
+// OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT 
+// SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, 
+// INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, 
+// PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT 
+// LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE 
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+// 
+// General Information:
+// xaifBooster is intended for the transformation of 
+// numerical programs represented as xml files according 
+// to the XAIF schema. It is part of the OpenAD framework. 
+// The main application is automatic 
+// differentiation, i.e. the generation of code for 
+// the computation of derivatives. 
+// The following people are the principal authors of the 
+// current version: 
+// 	Uwe Naumann
+//	Jean Utke
+// Additional contributors are: 
+//	Andrew Lyons
+//	Peter Fine
+//
+// For more details about xaifBooster and its use in OpenAD please visit:
+//   http://www.mcs.anl.gov/openad
+//
+// This work is partially supported by:
+// 	NSF-ITR grant OCE-0205590
+// ========== end copyright notice ==============
 #include <iostream>
 #include <utility>
-#include <typeinfo>
 
 #include "xaifBooster/utils/inc/DbgLoggerManager.hpp"
 
@@ -21,25 +72,27 @@ int main(int argc,char** argv) {
   DbgLoggerManager::instance()->setSelection(DbgGroup::ERROR 
 					     //| DbgGroup::CALLSTACK | DbgGroup::TEMPORARY
 					     );
-  xaifBoosterPushPop::AlgConfig algConfig(argc,argv,buildStamp);
+  xaifBoosterPushPop::AlgFactoryManager* theFManager_p=
+    dynamic_cast<xaifBoosterPushPop::AlgFactoryManager*>(xaifBoosterPushPop::AlgFactoryManager::instance());
+  theFManager_p->init();
+  xaifBoosterPushPop::AlgConfig* algConfig_p=theFManager_p->makeAlgConfig(argc,argv,buildStamp);
   try { 
-    algConfig.config();
+    algConfig_p->config();
   } catch (BaseException& e) { 
     DBG_MACRO(DbgGroup::ERROR,
 	      "caught exception: " << e.getReason());
-    algConfig.usage();
+    algConfig_p->usage();
     return -1;
   } // end catch 
   try {   
-    xaifBoosterPushPop::AlgFactoryManager::instance()->init();
     InlinableIntrinsicsParser ip(ConceptuallyStaticInstances::instance()->getInlinableIntrinsicsCatalogue());
     ip.initialize();
-    ip.setExternalSchemaLocation(algConfig.getSchemaPath());
-    ip.parse(algConfig.getIntrinsicsFileName());
+    ip.setExternalSchemaLocation(algConfig_p->getSchemaPath());
+    ip.parse(algConfig_p->getIntrinsicsFileName());
     XAIFBaseParser p;
-    p.initialize(algConfig.getInputValidationFlag());
-    p.setExternalSchemaLocation(algConfig.getSchemaPath());
-    p.parse(algConfig.getInputFileName());
+    p.initialize(algConfig_p->getInputValidationFlag());
+    p.setExternalSchemaLocation(algConfig_p->getSchemaPath());
+    p.parse(algConfig_p->getInputFileName());
     CallGraph& Cg(ConceptuallyStaticInstances::instance()->getCallGraph());
     Cg.getScopeTree().forcedPassivation();
     Cg.genericTraversal(GenericAction::ALGORITHM_ACTION_1); // linearize
@@ -50,13 +103,13 @@ int main(int argc,char** argv) {
     Cg.genericTraversal(GenericAction::ALGORITHM_ACTION_6); // place pushes and pops for required values
     const std::string& oldSchemaLocation(Cg.getSchemaLocation());
     std::string newLocation(oldSchemaLocation,0,oldSchemaLocation.find(' '));
-    if (algConfig.getSchemaPath().size())
-      newLocation.append(" "+algConfig.getSchemaPath()+"/xaif_output.xsd");
+    if (algConfig_p->getSchemaPath().size())
+      newLocation.append(" "+algConfig_p->getSchemaPath()+"/xaif_output.xsd");
     else 
       newLocation.append(" xaif_output.xsd");
     Cg.resetSchemaLocation(newLocation);
-    if (algConfig.isSet('o')) { 
-      std::ofstream theOutFile(algConfig.getOutFileName().c_str(),
+    if (algConfig_p->isSet('o')) { 
+      std::ofstream theOutFile(algConfig_p->getOutFileName().c_str(),
 			       std::ios::out);
       Cg.printXMLHierarchy(theOutFile);
       theOutFile.close();
