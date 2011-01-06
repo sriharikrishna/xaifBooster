904db4bfecfa1e31d1fc2037b4e6bfdd7f1c4bf6
utke <utkej1@gmail.com>
2011-01-06 01:28:38 -0600

    copyright notice updated in source files

--- algorithms/PushPop/src/CallGraphVertexAlgFactory.cpp
+++ algorithms/PushPop/src/CallGraphVertexAlgFactory.cpp
@@ -1,3 +1,13 @@
+// ========== begin copyright notice ===================
+// This file is part of
+// ---------------
+// xaifBooster
+// ---------------
+// which is distributed under the BSD license.
+// The full COPYRIGHT notice can be found in the top
+// level directory of the xaifBooster distribution.
+// ========== end copyright notice =====================
+
 #include "xaifBooster/system/inc/CallGraphVertexAlgBase.hpp"
 #include "xaifBooster/system/inc/CallGraphVertex.hpp"
 #include "xaifBooster/algorithms/PushPop/inc/CallGraphVertexAlgFactory.hpp"
--- system/src/BasicBlockElementAlgBase.cpp
+++ system/src/BasicBlockElementAlgBase.cpp
@@ -1,3 +1,13 @@
+// ========== begin copyright notice ===================
+// This file is part of
+// ---------------
+// xaifBooster
+// ---------------
+// which is distributed under the BSD license.
+// The full COPYRIGHT notice can be found in the top
+// level directory of the xaifBooster distribution.
+// ========== end copyright notice =====================
+
 #include "xaifBooster/system/inc/BasicBlockElementAlgBase.hpp"
 #include "xaifBooster/system/inc/BasicBlockElement.hpp"
 
