c437877c75ff46a836a5f0f0dae8617f334904d5
utke <utkej1@gmail.com>
2011-03-08 11:16:30 -0600

    refactor the macro

--- utils/inc/BaseException.hpp
+++ utils/inc/BaseException.hpp
@@ -47,7 +47,36 @@ namespace xaifBooster {
      */
     std::string myReason;
 
-  }; // end of class PrintingException 
+  };
+
+   /**
+   * use of preprocessor defined __FILE__ and __LINE__ requires
+   * use of a macro. We have to define a strange name
+   * for local variables to avoid accidental name matching -
+   * the niceties of macros!
+   * Note the enclosing {} allowing for
+   * if (...)
+   *   THROW_LOGICEXCEPTION_MACRO(...);
+   * without extra brackets but the compiler will complain
+   * about
+   * if (...) {
+   *   THROW_LOGICEXCEPTION_MACRO(...);
+   * }
+   * because it doesn't know what to do with the extra ';' in the end;
+   * make your choice.
+   * the  argument is an argument to an
+   * ostream << operator.
+   */
+#ifndef THROW_EXCEPTION_MACRO
+#define THROW_EXCEPTION_MACRO(className,StreamArgs) \
+   { \
+     std::ostringstream aLoNgAnDwEiRdLoCaLnAmeFoRtHiSmAcRoOnLy; \
+     aLoNgAnDwEiRdLoCaLnAmeFoRtHiSmAcRoOnLy << StreamArgs << std::ends; \
+     throw className(__FILE__,__LINE__,aLoNgAnDwEiRdLoCaLnAmeFoRtHiSmAcRoOnLy.str()); \
+   }
+#else
+#error macro name for THROW_LOGICEXCEPTION_MACRO already in use
+#endif
 
 } // end of namespace xaifBooster
                                                                      
